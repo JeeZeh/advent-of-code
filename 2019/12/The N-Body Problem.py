73d793da063349f55ffd3d71e35bcdd77e0e5a62
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2019-12-12 21:57:25 +0000

    Not working

---
+++ 2019/12/The N-Body Problem.py
@@ -0,0 +1,81 @@
+from collections import OrderedDict
+from pprint import pprint
+import numpy as np
+
+system = OrderedDict()
+initial = OrderedDict()
+init_np = None
+locks = {}
+
+
+def init():
+    global system, initial, init_np, locks
+    system = OrderedDict()
+    locks = {}
+    with open("input.txt") as f:
+        for line in f:
+            p = [int(p[2:]) for p in line.rstrip()[1:-1].split(", ")]
+            system[tuple(p)] = [0, 0, 0]
+    initial = system.copy()
+    init_np = np.array(list(initial.keys()))
+
+
+def step(curr_step):
+    global system
+    buffer = OrderedDict()
+
+    for m1, s1 in system.items():
+        for m2 in list(filter(lambda x: x != m1, system.keys())):
+            for a in range(3):
+                if m1[a] < m2[a]:
+                    s1[a] += 1
+                if m1[a] > m2[a]:
+                    s1[a] -= 1
+
+        m1 = list(m1)
+        for a in range(3):
+            m1[a] += s1[a]
+        buffer[tuple(m1)] = s1
+
+    system = buffer
+    for i in range(3):
+        if not np.array(list(system.values()))[:, i].any():
+            if i not in locks:
+                pprint(system)
+                print()
+                locks[i] = curr_step
+
+
+def state_equal(step):
+    return initial.keys() == system.keys()
+
+
+def simulate(steps):
+    init()
+
+    if steps == -1:
+        c = 1
+        step(c)
+        while len(locks) < 3:
+            step(c)
+            c += 1
+        print(c + 1)
+    else:
+        for i in range(steps):
+            step(i)
+
+
+def get_energy():
+    return sum(
+        [sum(map(abs, list(p))) * sum(map(abs, list(v))) for p, v in system.items()]
+    )
+
+
+simulate(100)
+pprint(system)
+
+simulate(-1)
+pprint(system)
+pprint(locks)
+v = list(locks.values())
+print(np.lcm(np.lcm(v[0], v[1]), v[2]))
