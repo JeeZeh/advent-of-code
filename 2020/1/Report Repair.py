b0829ccb84e57f417cccc0e371cc2ec5088dc414
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2020-12-01 21:32:35 +0000

    2020 Woo!

---
+++ 2020/1/Report Repair.py
@@ -0,0 +1,28 @@
+from itertools import combinations
+from math import prod
+
+numbers = list(map(int, open("input.txt").readlines()))
+ 
+numset = set(numbers)
+
+def sumset(s, n):
+    if s - n in numset:
+        return n, s - n
+
+def get_subsum(s):
+    for n in numbers:
+        if sol := sumset(s, n):
+            return(prod(sol))
+     
+print(get_subsum(2020))    
+
+for n in numbers:
+    if sol := get_subsum(2020 - n):
+        print(sol * n)
+        break
+        
+        
+ 
+# sumset = {sum(c): (c[0], c[1]) for c in combinations(numbers, r=2)}
+# print(sumset[2020][0] * sumset[2020][1])
+
