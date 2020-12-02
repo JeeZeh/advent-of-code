9f7dde19b03c97fcfb688e0ce7c100b7479f22d4
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2020-12-02 20:11:35 +0000

    2020-2 initial sol

---
+++ 2020/2/Password Philosophy.py
@@ -0,0 +1,27 @@
+passwords = [map(str.strip, line.split(":")) for line in open("input.txt").readlines()]
+
+
+def valid(mn, mx, c, p):
+    return mn <= p.count(c) <= mx
+
+def valid_2(p1, p2, c, p):
+    p1 -= 1
+    p2 -= 1
+    
+    return (p[p1] == c) ^ (p[p2] == c)
+
+v1_count = 0
+v2_count = 0
+
+for rule, password in passwords:
+    min_, max_ = map(int, rule[:-2].split("-"))
+    char = rule[-1]
+    
+    if valid(min_, max_, char, password):
+        v1_count += 1
+        
+    if valid_2(min_, max_, char, password):
+        v2_count += 1 
+        
+print(v1_count)
+print(v2_count)
