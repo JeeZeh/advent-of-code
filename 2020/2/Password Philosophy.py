9f0fde546fbb6d2a62add467e10a164213b49d2f
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2020-12-03 21:33:57 +0000

    Cursed eval

--- 2020/2/Password Philosophy.py
+++ 2020/2/Password Philosophy.py
@@ -4,11 +4,13 @@ passwords = [map(str.strip, line.split(":")) for line in open("input.txt").readl
 def valid(mn, mx, c, p):
     return mn <= p.count(c) <= mx
 
+
 def valid_2(p1, p2, c, p):
     p1 -= 1
     p2 -= 1
-    
+
     return (p[p1] == c) ^ (p[p2] == c)
+
 
 v1_count = 0
 v2_count = 0
@@ -16,12 +18,15 @@ v2_count = 0
 for rule, password in passwords:
     min_, max_ = map(int, rule[:-2].split("-"))
     char = rule[-1]
-    
+
     if valid(min_, max_, char, password):
         v1_count += 1
-        
+
     if valid_2(min_, max_, char, password):
-        v2_count += 1 
-        
+        v2_count += 1
+
 print(v1_count)
 print(v2_count)
+
+codes = [line.split() for line in open("input.txt").readlines()]
+print(sum([eval(c[0].replace("-", "<=c[2].count(c[1][0])<=")) for c in codes]))
