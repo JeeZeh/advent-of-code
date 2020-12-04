f1cee21060f32381d948a3eed8e85ac6e0cf2efc
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2020-12-04 02:09:15 +0000

    colours are not working :(

--- 2020/3/Toboggan Trajectory.py
+++ 2020/3/Toboggan Trajectory.py
@@ -1,21 +1,68 @@
+from time import sleep
 from math import prod
+import os
 
 course = open("input.txt").readlines()
-wrap = len(course[0]) - 1
-
+wrap_x = len(course[0]) - 1
+wrap_y = len(course) - 1
+max_x = len(course)+len(course[0])
 
 def get_treejectory(slope):
-    r, d = slope
-    trees, x, y = 0, 0, 0
+    right, down = slope
+    trees, x, y = [], 0, 0
     while y < len(course):
         if course[y][x] == "#":
-            trees += 1
+            trees.append((x, y))
 
-        x = (x + r) % wrap
-        y += d
+        x = (x + right) % wrap_x
+        y += down
 
     return trees
 
 
-print(get_treejectory((3, 1)))
-print(prod(map(get_treejectory, [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)])))
+# print(len(get_treejectory((3, 1))))
+# print(prod(map(get_treejectory, [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)])))
+
+
+def render_slice(width, height, center_x, center_y, pos_x, pos_y, slope):
+    trees = set()
+    y = pos_x - slope[1]
+    for x in reversed(range(center_x, pos_x, slope[0])):
+        if course[y][x % wrap_x] == "#":
+            trees.add((x, y))
+        y -= slope[1]
+    
+    for y in range(center_y, height + center_y):
+        row = []
+        for x in range(center_x, width + center_x):
+            char = "@" if (x,y) == (pos_x, pos_y) else course[y % wrap_y][x % wrap_x]
+            if (x, y) in trees:
+                char = f"\u001b[31m{char}\u001b[0m"
+            row.append(char)
+        print("".join(row))
+
+def cls():
+   # for mac and linux(here, os.name is 'posix')
+   if os.name == 'posix':
+      os.system('clear')
+   else:
+      # for windows platfrom
+      os.system('cls')
+   # print out some text
+
+def play_path(slope, width, height):
+    right, down = slope
+    sled_x, sled_y = 0, 0
+    offset_x, offset_y = width//2, height//2
+    max_x =  wrap_y*right - width - right + 1
+    max_y = wrap_y - offset_y*2
+    while sled_y < wrap_y:
+        cls()
+        origin_x = max(0, min(sled_x - offset_x, max_x))
+        origin_y = max(0, min(sled_y - offset_y, max_y))
+        render_slice(width, height, origin_x, origin_y, sled_x, sled_y, slope)
+        sleep(0.05)
+        sled_x += right
+        sled_y += down
+
+play_path((2, 1), 50, 20)
