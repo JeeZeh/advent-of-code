beac1e03783e480e58f7ad44f18c417d819d94b9
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2020-12-06 01:32:50 +0000

    Tidy

--- 2015/17/No Such Thing as Too Much.py
+++ 2015/17/No Such Thing as Too Much.py
@@ -1,10 +1,10 @@
 from itertools import combinations
-from collections import Counter
 
 buckets, MAX = [int(l) for l in open("input.txt")], 150
-combs = [combinations(buckets, i) for i in range(len(buckets))]
+
+combs = (combinations(buckets, i) for i in range(len(buckets)))
 
 success = [a for c in combs for a in c if sum(a) == MAX]
 
 print(len(success))
-print(len(list(filter(lambda c: len(c) == len(success[0]), success))))
+print(sum(map(lambda c: len(c) == len(success[0]), success)))
