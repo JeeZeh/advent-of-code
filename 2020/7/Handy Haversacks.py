302ec5e93ba475d8cb516e507102428ac46cc2b8
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2020-12-07 20:43:25 +0000

    black

--- 2020/7/Handy Haversacks.py
+++ 2020/7/Handy Haversacks.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from pprint import pprint
 
+
 def parse_bag(bag):
     parts = bag.split(" bags contain ")
     parent = parts[0]
@@ -19,34 +20,38 @@ bags = list(map(parse_bag, map(str.strip, open("input.txt").readlines())))
 
 nest = {parent: contains for parent, contains in bags}
 
+
 def inside_out(bags):
     out = defaultdict(list)
     for parent, contents in bags.items():
         for content in contents:
             out[content[1]].append((content[0], parent))
-            
+
     return out
+
 
 def get_hierarchy(inv_nest, colour):
     hierarchy = []
     if colour not in inv_nest:
         return hierarchy
-    
+
     for bag in inv_nest[colour]:
         hierarchy.append(bag[1])
         hierarchy.extend(get_hierarchy(inv_nest, bag[1]))
-    
+
     return hierarchy
+
 
 def get_nesting(nest, colour):
     required = 1
     if colour not in nest:
         return required
-    
+
     for bag in nest[colour]:
-        required += bag[0]*get_nesting(nest, bag[1])
-        
+        required += bag[0] * get_nesting(nest, bag[1])
+
     return required
+
 
 inverted_nest = inside_out(nest)
 
