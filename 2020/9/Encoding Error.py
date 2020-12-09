b3fb6fe5fbb0c2d614fb6eb22abdee9092ada683
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2020-12-09 21:45:34 +0000

    black

--- 2020/9/Encoding Error.py
+++ 2020/9/Encoding Error.py
@@ -9,20 +9,22 @@ def get_subsum(s, slice_):
         if s - n in numset:
             return n, s - n
 
+
 def generate_subset_sums():
     subsets = {}
-    
+
     for slice_length in range(2, len(data)):
         for i in range(0, len(data)):
-            slice_ = data[i: i+slice_length]
+            slice_ = data[i : i + slice_length]
             subsets[sum(slice_)] = slice_
-            
+
     return subsets
+
 
 subset_sums = generate_subset_sums()
 
 for i in range(window, len(data)):
-    if not get_subsum(data[i], data[i-window: i]):
+    if not get_subsum(data[i], data[i - window : i]):
         print(data[i])
         if sol := subset_sums.get(data[i]):
             print(min(sol) + max(sol))
