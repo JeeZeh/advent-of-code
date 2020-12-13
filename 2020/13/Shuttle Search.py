40faacde1d5407bd21ebe4cc0701fbde8a0d2016
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2020-12-13 19:07:00 +0000

    black

--- 2020/13/Shuttle Search.py
+++ 2020/13/Shuttle Search.py
@@ -4,18 +4,18 @@ import math
 earliest, busses_line = open("input.txt")
 earliest = int(earliest)
 busses = list(map(int, filter(lambda x: x != "x", busses_line.split(","))))
-              
+
 nearest = min((bus - (earliest % bus), bus) for bus in busses)
 print("Part 1:", nearest[0] * nearest[1])
 
-        
+
 busses = [(i, int(b)) for i, b in enumerate(busses_line.split(",")) if b != "x"]
 
-# This took way too long to figure out, I had been trying fancier things like lcm/gcd... 
+# This took way too long to figure out, I had been trying fancier things like lcm/gcd...
 # I had this idea of offsetting all the busses to line up with the first one and using LCM to get the first point that happens.
 # This is sort of how the solution works, but the offset is used in a different way than I had first tried,
 # and LCM isn't used (or maybe a principle of it actually is, not sure).
-# 
+#
 # Solution:
 #   a, b are 2 periods (busses, waves, etc.)
 #   Period 'a' has an offset i.e. the cycle begins at this time (-10 minutes, +3000 minutes, etc)
@@ -35,16 +35,18 @@ busses = [(i, int(b)) for i, b in enumerate(busses_line.split(",")) if b != "x"]
 #     4 -> len(busses) - 1: ...
 #     len(busses): The final evaluation gives us the time when the busses appear in the pattern we want
 
+
 def get_wave_params(a, b, diff, offset):
     check = a + offset
     while True:
         if (check + diff) % b == 0:
-            return check, a*b, check - a*b
+            return check, a * b, check - a * b
 
         check = check + a
-        
+
+
 freq, offset, cycle_start = busses[0][1], 0, 0
 for bus in busses[1:]:
     cycle_start, freq, offset = get_wave_params(freq, bus[1], bus[0], offset)
-    
+
 print("Part 2:", cycle_start)
