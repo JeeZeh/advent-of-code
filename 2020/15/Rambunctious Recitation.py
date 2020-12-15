b77572518cc8e7880ce560d05b8c3f86de88cf56
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2020-12-15 22:39:51 +0000

    format

--- 2020/15/Rambunctious Recitation.py
+++ 2020/15/Rambunctious Recitation.py
@@ -1,21 +1,23 @@
 start = "2,0,6,12,1,3"
 
+
 def get_turn(n):
-    mem = {int(v):i+1 for i, v in enumerate(start.split(",")[:-1])}
-    
+    mem = {int(v): i + 1 for i, v in enumerate(start.split(",")[:-1])}
+
     turn = len(mem)
     last = int(start[-1])
     next = None
-    while turn+1 != n:
+    while turn + 1 != n:
         turn += 1
         if last not in mem:
             next = 0
         else:
             next = turn - mem[last]
-        
+
         mem[last] = turn
         last = next
     print(last)
-            
+
+
 get_turn(2020)
 get_turn(30000000)
