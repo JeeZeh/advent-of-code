09d0be5f9f54c0145817ecefc1b9aca5034e2a48
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2020-12-19 01:04:10 +0000

    2020-17

--- 2020/17/Conway Cubes.py
+++ 2020/17/Conway Cubes.py
@@ -1,24 +1,33 @@
 from collections import defaultdict
 
-dirs = [(x, y, z, w) for w in range(-1, 2) for z in range(-1, 2) for y in range(-1, 2) for x in range(-1, 2) if (x, y, z, w) != (0,0,0,0)]
+dirs = [
+    (x, y, z, w)
+    for w in range(-1, 2)
+    for z in range(-1, 2)
+    for y in range(-1, 2)
+    for x in range(-1, 2)
+    if (x, y, z, w) != (0, 0, 0, 0)
+]
 
 pocket = open("real.txt").read().splitlines()
+
 
 def generate_space(layer):
     space = defaultdict(int)
     for w in range(-1, 2):
         for z in range(-1, 2):
-            for y in range(-1, len(layer)+1):
-                for x in range(-1, len(layer[0])+1):
+            for y in range(-1, len(layer) + 1):
+                for x in range(-1, len(layer[0]) + 1):
                     space[(x, y, z, w)] = 0
 
     for y, r in enumerate(layer):
         for x, c in enumerate(r):
-            space[(x, y, 0,  0)] = 1 if c == "#" else 0
-            
+            space[(x, y, 0, 0)] = 1 if c == "#" else 0
+
     return space
-            
-space = generate_space(pocket)            
+
+
+space = generate_space(pocket)
 
 
 def cycle(space):
@@ -31,17 +40,16 @@ def cycle(space):
             look_at = (x + dx, y + dy, z + dz, w + dw)
             if eval_space[look_at]:
                 surrounding.append(look_at)
-            
+
         if active and len(surrounding) not in (2, 3):
             state[(x, y, z, w)] = 0
         elif not active and len(surrounding) == 3:
             state[(x, y, z, w)] = 1
-            
-    eval_space |= state
-            
-    return eval_space
+
+    return eval_space | state
+
 
 for t in range(6):
     space = cycle(space)
-    
+
 print(sum(v for k, v in space.items()))
