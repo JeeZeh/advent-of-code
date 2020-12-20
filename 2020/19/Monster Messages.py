87a14a7f8ce7913a06a6269b262cc405b3dbc7c6
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2020-12-20 05:15:14 +0000

    2020-19 Part 1 - This took a LONG time

---
+++ 2020/19/Monster Messages.py
@@ -0,0 +1,65 @@
+from itertools import combinations, islice
+from pprint import pprint
+
+rules, messages = open("real.txt").read().split("\n\n")
+
+mem = {}
+
+for rule in rules.splitlines():
+    parts = rule.split(": ")
+    num = parts[0]
+    ref = ""
+    if '"' in parts[1]:
+        ref = parts[1][1]
+    else:
+        refs = parts[1].split(" | ")
+        ref = list(map(lambda x: tuple(x.split()), refs))
+        
+    mem[num] = ref
+    
+print(mem)
+
+
+def generate_choices(rule):
+    solutions = []
+    
+    if isinstance(mem[rule], str):
+        return mem[rule]
+    else:
+        for req in mem[rule]:
+            c = []
+            for r in req:
+                c.append(generate_choices(r))
+            solutions.append(tuple(c))
+        
+    return solutions
+
+
+def try_match(phrase, rule):
+    if isinstance(rule, tuple):
+        copy = phrase
+        for r in rule:
+            new_phrase = try_match(phrase, r)
+            if phrase == new_phrase:
+                return copy
+            else:
+                phrase = new_phrase
+        return phrase
+    if isinstance(rule, list):
+        copy = phrase
+        for r in rule:
+            new_phrase = try_match(phrase, r)
+            if new_phrase != phrase:
+                return new_phrase
+        return copy
+    if isinstance(rule, str):
+        if phrase[0] == rule:
+            return phrase[1:]
+        return phrase
+
+def is_valid(phrase, rule):
+    return not try_match(phrase, rule)
+        
+rule = generate_choices("0")[0]
+
+print(sum(is_valid(m, rule) for m in messages.splitlines()))
