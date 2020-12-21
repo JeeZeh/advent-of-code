20ac7e7a7d65353c4a76b9de9d96eb8ed42ad4d8
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2020-12-21 19:53:36 +0000

    2020-20 Part 2 :)

--- 2020/20/Jurassic Jigsaw.py
+++ 2020/20/Jurassic Jigsaw.py
@@ -1,8 +1,9 @@
+import enum
 from math import sqrt
 import numpy as np
 from math import prod
-
-from numpy.lib.type_check import imag
+from itertools import product
+import numpy as np
 
 tiles_raw = open("real.txt").read().split("\n\n")
 w, h = 10, 10
@@ -46,9 +47,9 @@ class Tile:
                 transforms.append(Tile(self.id_, self.tile).rotate(r).flip(f))
 
         return transforms
-    
+
     def strip_borders(self):
-        self.tile = self.tile[1:-1,1:-1]
+        self.tile = self.tile[1:-1, 1:-1]
         return self
 
 
@@ -139,7 +140,84 @@ def stitch_image(tiles):
                     {"grid": _grid, "unused_ids": _unused_ids, "step": step + 1}
                 )
 
+
 image, size = stitch_image(tiles)
+
+
+def crop_and_merge_image(image, size):
+    new_image = image.copy()
+
+    for pos, tile in image.items():
+        new_image[pos] = tile.strip_borders()
+
+    merged = np.concatenate(
+        tuple(
+            (np.concatenate(tuple(new_image[(x, y)].tile for x in range(size)), axis=1))
+            for y in range(size)
+        ),
+        axis=0,
+    )
+
+    return merged
+
+
+def is_wildcard(array):
+    def func(a):
+        return a == 0
+
+    return np.vectorize(func)(array)
+
+
+def find_monster_indexes(slice_range, monster_pattern):
+    y_slice, x_slice = slice_range
+
+    y_offset = y_slice[0]
+    x_offset = x_slice[0]
+
+    positions = set()
+    for y, row in enumerate(monster_pattern):
+        for x, m in enumerate(row):
+            if m == 1:
+                positions.add((y + y_offset, x + x_offset))
+
+    return positions
+
+
+# Thought about doing a sort-of convolution, found this: https://stackoverflow.com/a/52095255
+# Changed some parts to count 0 as a wildcard, and also to collect the indices that were matched
+def match_pattern(input_array, pattern, wildcard_function=is_wildcard):
+    pattern_shape = pattern.shape
+    input_shape = input_array.shape
+
+    is_wildcard = wildcard_function(pattern)  # This gets a boolean N-dim array
+
+    if len(pattern_shape) != len(input_shape):
+        raise ValueError("Input array and pattern must have the same dimension")
+
+    shape_difference = [i_s - p_s for i_s, p_s in zip(input_shape, pattern_shape)]
+
+    if any((diff < -1 for diff in shape_difference)):
+        raise ValueError("Input array cannot be smaller than pattern in any dimension")
+
+    dimension_iterators = [range(0, s_diff + 1) for s_diff in shape_difference]
+
+    matches = set()
+
+    # This loop will iterate over every possible "window" given the shape of the pattern
+    for start_indexes in product(*dimension_iterators):
+        indices = [
+            (start_i, start_i + p_s)
+            for start_i, p_s in zip(start_indexes, pattern_shape)
+        ]
+        range_indexes = tuple(map(lambda x: slice(*x), indices))
+        input_match_candidate = input_array[range_indexes]
+
+        # This checks that for the current "window" - the candidate - every element is equal
+        #  to the pattern OR the element in the pattern is a wildcard
+        if np.all(np.logical_or(is_wildcard, (input_match_candidate == pattern))):
+            matches |= find_monster_indexes(indices, pattern)
+
+    return matches
 
 
 ### Part 1 ###
@@ -150,7 +228,35 @@ def part_1(image, size):
         image[(size - 1, 0)],
         image[(size - 1, size - 1)],
     ]
-    
+
     print(prod(int(c.id_) for c in corners))
 
+
+monster = """                  # 
+#    ##    ##    ###
+ #  #  #  #  #  #   
+"""
+
+
+def part_2(image, size):
+    monster_pattern = np.array(
+        [[1 if c == "#" else 0 for c in line] for line in monster.splitlines()],
+        dtype=int,
+    )
+    search_space = Tile(0, crop_and_merge_image(image, size))
+
+    search_space.generate_transforms()
+
+    matches = [
+        (match_pattern(ss.tile, monster_pattern), ss.tile)
+        for ss in search_space.generate_transforms()
+    ]
+    monsters, space = list(filter(lambda x: x[0], matches))[0]
+    m_count = len(monsters)
+    
+    
+    print(space.sum() - m_count)
+
+
 part_1(image, size)
+part_2(image, size)
