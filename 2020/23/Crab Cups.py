f50138571957cb5b6bab7bdab8856d46b54c7249
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2020-12-23 19:46:22 +0000

    2020-23 Part 2 with Dictionary

--- 2020/23/Crab Cups.py
+++ 2020/23/Crab Cups.py
@@ -1,48 +1,95 @@
-cups = "389125467"
+cups = "193467258"
+
+cups_list = list(map(int, cups))
+
+cups = {}
+
+mn, mx = 1, len(cups_list)
+
+for i, c in enumerate(cups_list):
+    cups[c] = cups_list[(i + 1) % len(cups_list)]
 
 
-class clist(list):
-    def __getitem__(self, k):
-        if isinstance(k, slice):
-            start = k.start if k.start else 0
-            stop = k.stop if k.stop else len(self)
-            return [self[n % len(self)] for n in range(start, stop)]
-        return list.__getitem__(self, k % len(self))
+def move(current, cups):
+    next_3 = {
+        cups[current]: cups[cups[current]],
+        cups[cups[current]]: cups[cups[cups[current]]],
+        cups[cups[cups[current]]]: cups[cups[cups[cups[current]]]],
+    }
 
+    dest_cup = find_dest(current, cups, next_3)
 
-cups = clist(map(int, cups))
+    first_in_holding = cups[current]
+    last_in_holding = cups[cups[cups[current]]]
+    last_in_holding_points_to = cups[cups[cups[cups[current]]]]
 
-mx = max(cups)
-mn = min(cups)
+    # Current now points to the what the last cup in holding points to
+    cups[current] = last_in_holding_points_to
 
-def take(s, n, cups):
-    holding = cups[s : s + n]
-    cups = cups[s+n:len(cups)+s]
+    # The last cup in holding now points to what the destination points to
+    cups[last_in_holding] = cups[dest_cup]
 
-    return holding, cups
+    # The destination cup now points to the first cup in holding
+    cups[dest_cup] = first_in_holding
 
-
-def insert(to_insert, s, cups):
-    return clist(cups[:s] + to_insert + cups[s:])
+    return cups
 
 
 def find_dest(current, cups, holding):
     destination = current - 1
     while True:
-        if destination in cups:
-            return cups.index(destination), destination
         if destination < mn:
             destination = mx
-        if destination in holding:
+        elif destination in holding:
             destination -= 1
+        else:
+            return destination
 
 
-cup = cups[-1]
-for _ in range(100):
-    current_index = (cups.index(cup) + 1) % len(cups)
-    cup = cups[current_index]
-    holding, cups = take(current_index + 1, 3, cups)
-    insert_index, number = find_dest(cup, cups, holding)
-    cups = insert(holding, insert_index + 1, cups)
 
-print("".join(map(str,cups[cups.index(1) + 1 : cups.index(1) + len(cups)])))
+def part1(cups):
+    global mn, mx
+    mn, mx = 1, len(cups_list)
+
+    p1_cups = cups.copy()
+    current = cups_list[0]
+
+    for _ in range(100):
+        move(current, p1_cups)
+        current = p1_cups[current]
+
+    output = []
+    next_ = p1_cups[1]
+    while len(output) < len(p1_cups) - 1:
+        output.append(str(next_))
+        next_ = p1_cups[next_]
+
+    print("".join(output))
+
+
+def part2(cups):
+    global mn, mx
+
+    p2_cups = cups.copy()
+
+    for x in range(mx + 1, 1_000_001):
+        if x == 1_000_000:
+            p2_cups[x] = cups_list[0]
+        else:
+            p2_cups[x] = x + 1
+
+    p2_cups[cups_list[-1]] = mx + 1
+
+    mn, mx = 1, 1_000_000
+
+    current = cups_list[0]
+    for _ in range(10_000_000):
+        move(current, p2_cups)
+        current = p2_cups[current]
+
+    print(p2_cups[1], p2_cups[p2_cups[1]])
+    print(p2_cups[1] * p2_cups[p2_cups[1]])
+
+
+part1(cups)
+part2(cups)
