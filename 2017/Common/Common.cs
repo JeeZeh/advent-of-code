64c925d8dc94df759d725d529619adb97c243b84
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2021-03-13 19:39:07 +0000

    2017-3: Over-engineered naive C# solution

---
+++ 2017/Common/Common.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Common
+{
+    public class DefaultDictionary<TKey, TValue> : Dictionary<TKey, TValue>
+    {
+        Func<TValue> _init;
+
+        public DefaultDictionary(Func<TValue> init)
+        {
+            _init = init;
+        }
+
+        public new TValue this[TKey k]
+        {
+            get
+            {
+                if (!ContainsKey(k))
+                    Add(k, _init());
+                return base[k];
+            }
+            set => base[k] = value;
+        }
+    }
+
+    public struct Position
+    {
+        public readonly int x, y;
+
+        public Position(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Position Add(Position o)
+        {
+            return new Position(this.x + o.x, this.y + o.y);
+        }
+
+        public override int GetHashCode()
+        {
+            return (this.x, this.y).GetHashCode();
+        }
+
+
+        public override bool Equals(Object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Position p = (Position)obj;
+                return (x == p.x) && (y == p.y);
+            }
+        }
+
+        public bool Equals(Position o)
+        {
+            return this.x == o.x && this.y == o.y;
+        }
+    }
+}
---
+++ 2017/Day3/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Linq;
+using Common;
+
+
+namespace Day3
+{
+    class Day3
+    {
+        private static DefaultDictionary<Position, int> grid;
+
+        static (Position, int)[] spiralDirections = {
+            (new Position(1, 0), 1),
+            (new Position(0, 1), 0),
+            (new Position(-1, 0), 1),
+            (new Position(0, -1), 0),
+        };
+        static Position[] diagonals = {
+            new Position(0,1),
+            new Position(1,1),
+            new Position(1,0),
+            new Position(1,-1),
+            new Position(0,-1),
+            new Position(-1, -1),
+            new Position(-1, 0),
+            new Position(-1, 1),
+        };
+        static void Main(string[] args)
+        {
+            var input = int.Parse(Resources.input);
+
+            Console.WriteLine($@"Part 1: {PartOne(input)}");
+            Console.WriteLine($@"Part 2: {PartTwo(input)}");
+        }
+
+        static int PartOne(int searching)
+        {
+            SpiralFillUntil(() => grid.ContainsValue(searching), IncrementFill);
+            Position match = grid.First(pair => pair.Value == searching).Key;
+
+            return Math.Abs(match.x) + Math.Abs(match.y);
+        }
+
+        static int PartTwo(int searching)
+        {
+            SpiralFillUntil(() => grid.Values.Max() > searching, SurroundingSumFill);
+
+            return grid.First(pair => pair.Value > searching).Value;
+        }
+
+        static void SpiralFillUntil(Func<bool> predicate, Func<Position, int> fill)
+        {
+            Position pos = InitGrid();
+            var distance = 0;
+
+            while (!predicate())
+            {
+                foreach (var (dir, distanceIncrement) in spiralDirections)
+                {
+                    distance += distanceIncrement;
+                    pos = Walk(pos, dir, distance, fill);
+                }
+            }
+        }
+
+        static Position Walk(Position current, Position direction, int distance, Func<Position, int> fill)
+        {
+            for (var i = 0; i < distance; i++)
+            {
+                current = current.Add(direction);
+                grid[current] = fill(current);
+            }
+
+            return current;
+        }
+
+        static int IncrementFill(Position pos)
+        {
+            return grid.Keys.Count + 1;
+        }
+
+        static int SurroundingSumFill(Position pos)
+        {
+            int sum = 0;
+            foreach (var dir in diagonals)
+            {
+                sum += grid[pos.Add(dir)];
+            }
+
+            return sum;
+        }
+
+        static Position InitGrid()
+        {
+            Position center = new Position(0, 0);
+            grid = new DefaultDictionary<Position, int>(() => 0);
+            grid[center] = 1;
+
+            return center;
+        }
+
+        static int GetLevel(double pos)
+        {
+            var ceil = Math.Ceiling(Math.Sqrt(pos));
+            return (int)(ceil % 2 == 0 ? ceil : ceil - 1) / 2;
+        }
+    }
+}
