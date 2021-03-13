e501bfcc8b612e8d814376950b5527927e5175eb
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2021-03-13 19:56:04 +0000

    Good formatting

--- 2017/Day3/Program.cs
+++ 2017/Day3/Program.cs
@@ -9,56 +9,32 @@ namespace Day3
     {
         private static DefaultDictionary<Position, int> grid;
 
-        static (Position, int)[] spiralDirections = {
-            (new Position(1, 0), 1),
-            (new Position(0, 1), 0),
-            (new Position(-1, 0), 1),
-            (new Position(0, -1), 0),
+        static (Position, int)[] spiralDirections = { (new Position(1, 0), 1), (new Position(0, 1), 0), (new Position(-1, 0), 1), (new Position(0, -1), 0),
         };
-        static Position[] diagonals = {
-            new Position(0,1),
-            new Position(1,1),
-            new Position(1,0),
-            new Position(1,-1),
-            new Position(0,-1),
-            new Position(-1, -1),
-            new Position(-1, 0),
-            new Position(-1, 1),
+        static Position[] diagonals = { new Position(0,1), new Position(1,1), new Position(1,0), new Position(1,-1), new Position(0,-1), new Position(-1, -1), new Position(-1, 0), new Position(-1, 1),
         };
         static void Main(string[] args)
         {
-            var input = int.Parse(Resources.input);
-
-            Console.WriteLine($@"Part 1: {PartOne(input)}");
-            Console.WriteLine($@"Part 2: {PartTwo(input)}");
+            var input = int.Parse(Resources.input); Console.WriteLine($@"Part 1: {PartOne(input)}"); Console.WriteLine($@"Part 2: {PartTwo(input)}");
         }
 
         static int PartOne(int searching)
         {
-            SpiralFillUntil(() => grid.ContainsValue(searching), IncrementFill);
-            Position match = grid.First(pair => pair.Value == searching).Key;
-
-            return Math.Abs(match.x) + Math.Abs(match.y);
+            SpiralFillUntil(() => grid.ContainsValue(searching), IncrementFill); Position match = grid.First(pair => pair.Value == searching).Key; return Math.Abs(match.x) + Math.Abs(match.y);
         }
 
         static int PartTwo(int searching)
         {
-            SpiralFillUntil(() => grid.Values.Max() > searching, SurroundingSumFill);
-
-            return grid.First(pair => pair.Value > searching).Value;
+            SpiralFillUntil(() => grid.Values.Max() > searching, SurroundingSumFill); return grid.First(pair => pair.Value > searching).Value;
         }
 
         static void SpiralFillUntil(Func<bool> predicate, Func<Position, int> fill)
         {
-            Position pos = InitGrid();
-            var distance = 0;
-
-            while (!predicate())
+            Position pos = InitGrid(); var distance = 0; while (!predicate())
             {
                 foreach (var (dir, distanceIncrement) in spiralDirections)
                 {
-                    distance += distanceIncrement;
-                    pos = Walk(pos, dir, distance, fill);
+                    distance += distanceIncrement; pos = Walk(pos, dir, distance, fill);
                 }
             }
         }
@@ -67,10 +43,8 @@ namespace Day3
         {
             for (var i = 0; i < distance; i++)
             {
-                current = current.Add(direction);
-                grid[current] = fill(current);
+                current = current.Add(direction); grid[current] = fill(current);
             }
-
             return current;
         }
 
@@ -81,22 +55,12 @@ namespace Day3
 
         static int SurroundingSumFill(Position pos)
         {
-            int sum = 0;
-            foreach (var dir in diagonals)
-            {
-                sum += grid[pos.Add(dir)];
-            }
-
-            return sum;
+            int sum = 0; foreach (var dir in diagonals) sum += grid[pos.Add(dir)]; return sum;
         }
 
         static Position InitGrid()
         {
-            Position center = new Position(0, 0);
-            grid = new DefaultDictionary<Position, int>(() => 0);
-            grid[center] = 1;
-
-            return center;
+            Position center = new Position(0, 0); grid = new DefaultDictionary<Position, int>(() => 0); grid[center] = 1; return center;
         }
     }
 }
