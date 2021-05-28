d94bc50857aae74b043a57e92d7c90ff0ee4c3f4
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2021-05-28 22:39:31 +0100

    Absurdist

--- 2017/Day13/day13.py
+++ 2017/Day13/day13.py
@@ -4,15 +4,16 @@ from math import prod
 walls = list(map(lambda x: tuple(map(int, x.split(": "))), open("Day13/input")))
 
 
-def pass_through(starting):
-    return [w for w in walls if ( (w[0] + starting) % ((2 * w[1])-2)) == 0]
-    
+def pass_through(s):
+    iter_ = (w for w in walls if ((w[0] + s) % ((2 * w[1]) - 2)) == 0)
+
+    return any(iter_) if s else list(iter_)
+
 
 print(f"Part 1: {sum(map(prod, pass_through(0)))}")
 
-# Naive
+
 n = 0
-while pass_through(n):
-    n += 1
-    
+while pass_through(n := n + 1): pass
+
 print(f"Part 2: {n} picoseconds")
