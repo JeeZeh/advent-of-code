8ce1ed1c018ce797de65c1d44b9f0f72c607fd71
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2021-06-12 23:20:35 +0100

    Parsing works!

--- 2018/day04/src/main.rs
+++ 2018/day04/src/main.rs
@@ -1,16 +1,19 @@
 use chrono::prelude::*;
 use std::{fs, str::FromStr, string::ParseError};
 
+#[derive(Debug)]
 
 enum EntryType {
     Wake,
     Sleep,
     Rotate,
 }
+
+#[derive(Debug)]
 struct Entry {
     entry_type: EntryType,
     timestamp: DateTime<Utc>,
-    guard: Option<String>
+    guard: Option<String>,
 }
 
 impl FromStr for Entry {
@@ -19,12 +22,12 @@ impl FromStr for Entry {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut parts = s[1..].split("] ");
         let timestamp = Utc
-        .datetime_from_str(parts.next().unwrap(), "%Y-%m-%d %H:%M")
-        .unwrap();
+            .datetime_from_str(parts.next().unwrap(), "%Y-%m-%d %H:%M")
+            .unwrap();
 
         let entry = parts.next().unwrap();
         let entry_type: EntryType;
-        let guard: String;
+        let mut guard: Option<String> = None;
 
         if entry.contains("wake") {
             entry_type = EntryType::Wake;
@@ -32,18 +35,20 @@ impl FromStr for Entry {
             entry_type = EntryType::Sleep;
         } else {
             entry_type = EntryType::Rotate;
-            guard = String::from(&entry.split(" ").nth(1).unwrap()[1..]);
+            guard = Some(String::from(&entry.split(" ").nth(1).unwrap()[1..]));
         }
 
-        let guard = Some(guard);
-
-        Ok(Entry { entry_type, timestamp, guard})
+        Ok(Entry {
+            entry_type,
+            timestamp,
+            guard,
+        })
     }
 }
 
 fn main() {
     let file = fs::read_to_string("./src/sample").unwrap();
-    let entry: Entry = file.lines().nth(0).unwrap().parse().unwrap();
-
-    println!("{}", entry.timestamp)
+    let mut entries: Vec<Entry> = file.lines().map(|l| l.parse().unwrap()).collect();
+    entries.sort_by(|a, b| (&a.timestamp).partial_cmp(&b.timestamp).unwrap());
+    entries.iter().for_each(|f| println!("{:?}", f));
 }
