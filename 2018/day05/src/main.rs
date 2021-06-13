49a5b9faa4c975720c80b6de9552464951eaf17b
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2021-06-13 21:27:02 +0100

    2018:05 init

---
+++ 2018/day05/src/main.rs
@@ -0,0 +1,15 @@
+use std::{fs, iter::Enumerate};
+
+fn main() {
+    let file = fs::read_to_string("./src/sample").unwrap();
+    
+    
+}
+
+fn collapse(seq: &String) -> String {
+    let new_string = String::new();
+
+    seq.chars().enumerate().filter()
+
+    new_string
+}
