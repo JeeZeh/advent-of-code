8f941f409056eb905d2556d9d6e8f690dba698ae
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2021-06-28 20:21:48 +0100

    Funky caching

--- 2018/day08/src/main.rs
+++ 2018/day08/src/main.rs
@@ -1,35 +1,28 @@
-use std::{fs, iter::Peekable, str::Split, time::Instant};
+use std::{collections::HashMap, fs, iter::Peekable, str::Split, time::Instant};
 
-struct Header {
-    n_child: i32,
-    n_meta: i32,
-}
+use fn_cache::{FnCache, HashCache};
 
+#[derive(Eq, Hash)]
 struct Node {
-    header: Header,
+    id: usize,
     children: Vec<Node>,
     metadata: Vec<i32>,
 }
 
-impl Node {
-    fn new() -> Node {
-        Node {
-            header: Header {
-                n_child: 0,
-                n_meta: 0,
-            },
-            children: Vec::new(),
-            metadata: Vec::new(),
-        }
+impl PartialEq for Node {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
     }
 }
 
 fn main() {
     let instant = Instant::now();
 
-    let license: Node = parse_license(fs::read_to_string("./src/input").unwrap());
+    let license: Node = parse_license(&fs::read_to_string("./src/input").unwrap());
+
     println!("Part 1: {}", part_one(&license));
-    println!("Part 1: {}", part_two(&license));
+
+    println!("Part 2: {}", part_two(&license));
 
     println!("{}ms", instant.elapsed().as_micros() as f32 / 1000.0)
 }
@@ -40,36 +33,53 @@ fn part_one(node: &Node) -> i32 {
 }
 
 fn part_two(node: &Node) -> i32 {
+    let len_children = node.children.len();
+
+    if node.children.len() == 0 {
+        return node.metadata.iter().sum();
+    }
+
+    let mut cache_lol = [-1; 11];
+
+    let iter = node // Otherwise, each metadata entry
+        .metadata // refers to a child whose value
+        .iter() // should be calculated and summed.
+        .map(|m| (m - 1) as usize) // Index of child = (metadata - 1)
+        .filter(|m| *m < len_children); // Only valid indexes allowed
+
+    let mut sum: i32 = 0;
+
     // What is the value of a node?
-    match node.children.len() {
-        0 => node.metadata.iter().sum::<i32>(), // If no children, sum of its metadata
-        t => node // Otherwise, each metadata entry
-            .metadata // refers to a child whose value
-            .iter() // should be calculated and summed.
-            .map(|m| m - 1) // Index of child = (metadata - 1)
-            .filter(|m| *m < t as i32) // Only valid indexes allowed
-            .map(|idx| part_two(&node.children[idx as usize])) // Get that child's value
-            .sum::<i32>(),
-    }
-}
+    for idx in iter {
+        if cache_lol[idx] == -1 {
+            cache_lol[idx] = part_two(&node.children[idx])
+        }
 
-fn parse_license(file: String) -> Node {
-    let mut parts = file.split(" ").peekable();
-
-    parse_tree(&mut parts)
-}
-
-fn parse_tree(stream: &mut Peekable<Split<&str>>) -> Node {
-    let mut node = Node::new();
-
-    node.header.n_child = stream.next().unwrap().parse().unwrap();
-    node.header.n_meta = stream.next().unwrap().parse().unwrap();
-
-    for _ in 0..node.header.n_child {
-        node.children.push(parse_tree(stream))
+        sum += cache_lol[idx];
     }
 
-    for _ in 0..node.header.n_meta {
+    sum
+}
+
+fn parse_license(file: &str) -> Node {
+    parse_tree(0, &mut file.split(" "))
+}
+
+fn parse_tree<'a>(id: usize, stream: &mut impl Iterator<Item = &'a str>) -> Node {
+    let child_count = stream.next().unwrap().parse().unwrap();
+    let metadata_count = stream.next().unwrap().parse().unwrap();
+
+    let mut node = Node {
+        id: id,
+        children: Vec::with_capacity(child_count),
+        metadata: Vec::with_capacity(metadata_count),
+    };
+
+    for child_id in 0..child_count {
+        node.children.push(parse_tree(id + child_id + 1, stream));
+    }
+
+    for _ in 0..metadata_count {
         node.metadata.push(stream.next().unwrap().parse().unwrap());
     }
 
