53e9ab5ed2e99cf5028c10c8df3a6898f7444e35
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2021-09-05 13:32:06 +0100

    2015:7 — finally

--- 2015/7/Some Assembly Required.py
+++ 2015/7/Some Assembly Required.py
@@ -1,15 +1,13 @@
 from collections import defaultdict
 
-circuit = map(lambda x: x.split(" -> "), map(str.strip, open("input.txt")))
-wires = defaultdict(int)
-
 ops = {
-    "AND": lambda x, y: wires[x] & wires[y] % (2 ** 16),
-    "OR": lambda x, y: wires[x] | wires[y] % (2 ** 16),
-    "LSHIFT": lambda x, y: wires[x] << int(y) % (2 ** 16),
-    "RSHIFT": lambda x, y: wires[x] >> int(y) % (2 ** 16),
-    "NOT": lambda x: ~wires[x] % 2 ** 16
+    "AND": lambda x, y: (x & y) % (2 ** 16),
+    "OR": lambda x, y: (x | y) % (2 ** 16),
+    "LSHIFT": lambda x, y: (x << y) % (2 ** 16),
+    "RSHIFT": lambda x, y: (x >> y) % (2 ** 16),
+    "NOT": lambda x: ~x % (2 ** 16),
 }
+
 
 def is_int(x):
     try:
@@ -19,15 +17,49 @@ def is_int(x):
         return False
 
 
+def resolve_input_to_value(i: str):
+    global gates
+
+    if is_int(i):
+        return int(i)
+
+    if i in ops:
+        return i
+
+    input_ = gates[i]
+
+    if isinstance(input_, int):
+        return input_
+
+    input_ = list(map(resolve_input_to_value, input_))
+
+    if len(input_) == 1:
+        gates[i] = input_[0]
+    elif len(input_) == 2:
+        gates[i] = ops[input_[0]](input_[1])
+    else:
+        gates[i] = ops[input_[1]](input_[0], input_[2])
+
+    return gates[i]
+
+
+circuit = list(map(lambda x: x.split(" -> "), map(str.strip, open("input.txt"))))
+gates = defaultdict(int)
 for part in circuit:
     inputs = part[0].split()
     output = part[1]
-    print(inputs, output)
-    if len(inputs) == 1:
-        wires[output] =  int(inputs[0]) if is_int(inputs[0]) else wires.get(inputs[0])
-    elif len(inputs) == 2:
-        wires[output] = ops[inputs[0]](inputs[1])
-    else:
-        wires[output] = ops[inputs[1]](inputs[0], inputs[2])
+    gates[output] = inputs
 
-print(wires["a"])
+resolve_input_to_value("a")
+b_override = gates["a"]
+print(b_override)
+
+gates = defaultdict(int)
+for part in circuit:
+    inputs = part[0].split()
+    output = part[1]
+    gates[output] = inputs
+
+gates["b"] = b_override
+resolve_input_to_value("a")
+print(gates["a"])
