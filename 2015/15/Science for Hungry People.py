57e3094d89029b659ca914d5717fe3f2e72e8c36
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2021-09-26 20:39:36 +0100

    2015:15 - solved

--- 2015/15/Science for Hungry People.py
+++ 2015/15/Science for Hungry People.py
@@ -12,31 +12,57 @@ class Properties:
     calories: int
 
 
-def get_score(*args: List[Tuple[int, Properties]]):
+def parse(line: str) -> Properties:
+    parts = line.replace(",", "").split()
+
+    return Properties(
+        int(parts[2]),
+        int(parts[4]),
+        int(parts[6]),
+        int(parts[8]),
+        int(parts[10]),
+    )
+
+
+def get_score(*args: List[Tuple[int, Properties]]) -> Tuple[int, int]:
     capacity = []
     durability = []
     flavour = []
     texture = []
+    calories = []
 
     for amount, ingredient in args:
         capacity.append(amount * ingredient.capacity)
         durability.append(amount * ingredient.durability)
         flavour.append(amount * ingredient.flavour)
         texture.append(amount * ingredient.texture)
-        
-    print(capacity, durability, flavour, texture)
+        calories.append(amount * ingredient.calories)
 
-    return prod(
-        map(lambda x: max(0, x), map(sum, (capacity, durability, flavour, texture))),
+    return (
+        prod(
+            map(
+                lambda x: max(0, x), map(sum, (capacity, durability, flavour, texture))
+            ),
+        ),
+        sum(calories),
     )
 
 
+def constrained_values(n):
+    """
+    This is horrible. I know how to constrain the max for each range, 
+    but I've no idea how to constrain the start value.
+    """
+    for a in range(0, n + 1):
+        for b in range(0, n - a + 1):
+            for c in range(0, n - a - b + 1):
+                for d in range(0, n - a - b - c + 1):
+                    if a + b + c + d == n:
+                        yield (a, b, c, d)
 
-butterscotch = Properties(-1, -2, 6, 3, 8)
-cinnamon = Properties(2, 3, -2, -1, 3)
 
+ingredients = list(map(parse, open("input.txt")))
+variations = [get_score(*zip(v, ingredients)) for v in constrained_values(100)]
 
-print(get_score((44, butterscotch)))
-
-# for i in range(100, -1, -1):
-#     print(get_score((i, butterscotch)))
+print("Part 1:", max(variations)[0])
+print("Part 2:", max(filter(lambda v: v[1] == 500, variations))[0])
