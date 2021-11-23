dc7444d3c9e7c70a9ef5efeb7c55f3f1e5f9afff
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2021-11-23 01:21:23 +0000

    2019:22 - Reversing indexes works for part 1

--- 2019/22/Slam Shuffle.py
+++ 2019/22/Slam Shuffle.py
@@ -1,36 +1,77 @@
 from collections import deque
 
-deck = [c for c in range(119315717514047)]
+deck = [c for c in range(10007)]
 
-def deal():
-    global deck
+
+def deal(deck):
     deck = deck[::-1]
     return deck
 
-def cut(n):
-    global deck
-    deck = deck[n:] + deck[:n] 
+
+def reverse_deal(ptr: int, decksize: int):
+    ptr = decksize - ptr - 1
+    return ptr
+
+
+def cut(deck, n):
+    deck = deck[n:] + deck[:n]
     return deck
 
 
-def incr_deal(n):
-    global deck
+def reverse_cut(n: int, ptr: int, decksize: int):
+    return (ptr + n) % decksize
+
+
+def incr_deal(deck, n):
     deck = deck[::-1]
     table = [0 for _ in range(len(deck))]
     i = 0
     while deck:
-        table[i%len(table)] = deck.pop()
-        i+=n
+        table[i % len(table)] = deck.pop()
+        i += n
 
     deck = table
     return deck
 
-for line in open("input.txt"):
-    if "increment" in line:
-        incr_deal(int(line.split(" ")[-1]))
-    elif "cut" in line:
-        cut(int(line.split(" ")[-1]))
-    else:
-        deal()
 
-print(deck.index(2019))
+def reverse_incr_deal(n: int, ptr: int, decksize: int):
+    return (ptr * pow(n, -1, mod=decksize)) % decksize
+
+
+inputs = list(open("input.txt"))
+
+
+def position_of_card(card, deck):
+    for line in inputs:
+        if "increment" in line:
+            deck = incr_deal(deck, int(line.split(" ")[-1]))
+        elif "cut" in line:
+            deck = cut(deck, int(line.split(" ")[-1]))
+        else:
+            deck = deal(deck)
+
+    return deck.index(card)
+
+
+def number_of_card(position, decksize):
+    # Start with the position of the card after shuffling
+    ptr = position
+
+    # Follow each step in reverse to undo the shuffling
+    for line in reversed(inputs):
+        if "increment" in line:
+            ptr = reverse_incr_deal(int(line.split(" ")[-1]), ptr, decksize)
+        elif "cut" in line:
+            ptr = reverse_cut(int(line.split(" ")[-1]), ptr, decksize)
+        else:
+            ptr = reverse_deal(ptr, decksize)
+
+    # The pointer is now at its starting position. Since the positions of the deck
+    # are ordered, this is the number of the card
+    return ptr
+
+
+position = position_of_card(2019, deck)
+print(f"2019 is at position {position}")
+
+print(f"{number_of_card(position, 10007)} is at position {position}")
