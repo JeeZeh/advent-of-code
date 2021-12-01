10c13d154822d0dd7b6bde9fbaec4d0525965f13
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2021-12-01 22:24:31 +0000

    2021:01 Excessively cursed

--- 2021/day01_python/main.py
+++ 2021/day01_python/main.py
@@ -1,4 +1,2 @@
-d = list(map(int, open("in")))
-
-print(sum(d[i] < d[i + 1] for i in range(len(d) - 1)))
-print(sum(sum(d[i : i + 3]) < sum(d[i + 1 : i + 4]) for i in range(len(d) - 3)))
+print(sum(a < b for a, b in zip([""] + list(open("i")), open("i"))))
+print(sum(sum(a[:3]) < sum(a[1:]) for a in zip(*tuple(list(map(int, ["999"] * i + list(open("i")))) for i in range(3, -1, -1)))))
