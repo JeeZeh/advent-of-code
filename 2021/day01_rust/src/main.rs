c3669cafb251b42c624cbcb2a18c8ac9bff87064
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2021-12-02 00:05:08 +0000

    2021:01 Rust tidier

--- 2021/day01_rust/src/main.rs
+++ 2021/day01_rust/src/main.rs
@@ -7,38 +7,17 @@ fn main() {
         .map(|line| line.parse().unwrap())
         .collect();
 
-    println!("Part 1: {}", get_depth_changes(&input));
+    println!("Part 1: {}", get_sliding_sums(&input, 1));
     println!("Part 2: {}", get_sliding_sums(&input, 3));
 }
 
-fn get_sliding_sums(readings: &Vec<i32>, window_size: usize) -> i32 {
-    assert!(readings.len() >= window_size);
-
-    let mut increases = 0;
-    let mut last = readings[0..window_size].iter().sum();
-
-    for i in 1..readings.len() - window_size + 1 {
-        let sum: i32 = readings[i..i + window_size].iter().sum();
-        if sum > last {
-            increases += 1;
-        }
-        last = sum;
-    }
-
-    increases
-}
-
-fn get_depth_changes(readings: &Vec<i32>) -> i32 {
-    let mut increases = 0;
-    let mut last = readings.first().unwrap();
-
-    for reading in readings {
-        if reading > last {
-            increases += 1;
-        }
-
-        last = reading;
-    }
-
-    increases
+fn get_sliding_sums(readings: &Vec<i32>, window_size: usize) -> usize {
+    readings
+        .windows(window_size)
+        .map(|w| w.iter().sum())
+        .collect::<Vec<i32>>()
+        .as_slice()
+        .windows(2)
+        .filter(|w| w.first() < w.last())
+        .count()
 }
