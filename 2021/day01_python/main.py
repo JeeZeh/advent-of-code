7f78986444ea9d8a500ebdc37778acdd8fb8b5d9
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2021-12-02 01:45:23 +0000

    golf

--- 2021/day01_python/main.py
+++ 2021/day01_python/main.py
@@ -1,4 +1,4 @@
-depths = list(open("i"))
+d = list(open("i"))
 
-print(sum(a < b for a, b in zip([""] + depths, depths)))
-print(sum(a[0] < a[-1] for a in zip([""] * 3 + depths, depths)))
+print(sum(a < b for a, b in zip([""] + d, d)))
+print(sum(a[0] < a[-1] for a in zip([""] * 3 + d, d)))
