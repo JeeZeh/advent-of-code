de8f989a4505fd9ee7fca606844e2f8d3efcd34e
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2021-12-02 01:38:02 +0000

    2021:01 Turns out you can just look at the first and last slice indeces, since the two slices share two common elements

--- 2021/day01_python/main.py
+++ 2021/day01_python/main.py
@@ -1,2 +1,2 @@
 print(sum(a < b for a, b in zip([""] + list(open("i")), open("i"))))
-print(sum(sum(a[:3]) < sum(a[1:]) for a in zip(*tuple(list(map(int, ["999"] * i + list(open("i")))) for i in range(3, -1, -1)))))
+print(sum(a[0] < a[-1] for a in zip([""] * 3 + list(open("i")), list(open("i")))))
