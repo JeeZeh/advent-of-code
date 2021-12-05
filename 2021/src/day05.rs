34f27ffc79dcdd1e90dd98c38b35126cec9093e4
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2021-12-05 16:56:25 +0000

    Just use points in a dictionary instead of sets or point intersections

--- 2021/src/day05.rs
+++ 2021/src/day05.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::HashMap, f64::EPSILON, num::ParseIntError, str::FromStr, string::ParseError,
+    borrow::Borrow, collections::HashMap, num::ParseIntError, str::FromStr, string::ParseError,
 };
 
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
@@ -19,17 +19,10 @@ impl FromStr for Point {
     }
 }
 
-impl Point {
-    fn euclid_distance(&self, other: &Self) -> f64 {
-        (((self.x - other.x).pow(2) + (self.y - other.y).pow(2)) as f64).sqrt()
-    }
-}
-
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Debug)]
 struct Line {
     a: Point,
     b: Point,
-    length: f64,
 }
 
 impl FromStr for Line {
@@ -39,115 +32,59 @@ impl FromStr for Line {
         let parts: Vec<&str> = s.split(" -> ").collect();
         let a: Point = Point::from_str(parts[0]).unwrap();
         let b: Point = Point::from_str(parts[1]).unwrap();
-        let length = a.euclid_distance(&b);
-        Ok(Line { a, b, length })
+
+        Ok(Line { a, b })
     }
 }
 
 impl Line {
-    fn intersects(&self, point: &Point) -> bool {
-        let diff = self.a.euclid_distance(&point) + self.b.euclid_distance(&point) - self.length;
-        -0.000005 < diff && diff < 0.000005 // Horrible precision errors
+    fn get_points_iter(&self) -> Box<dyn Iterator<Item = Point> + '_> {
+        let (a, b) = (self.a.borrow(), self.b.borrow());
+
+        if a.x == b.x {
+            return Box::new((a.y.min(b.y)..=a.y.max(b.y)).map(|y| Point { x: a.x, y }));
+        } else if a.y == b.y {
+            return Box::new((a.x.min(b.x)..=a.x.max(b.x)).map(|x| Point { x, y: a.y }));
+        } else {
+            return Box::new((0..=(a.x - b.x).abs()).map(|d| Point {
+                x: if a.x > b.x { a.x - d } else { a.x + d },
+                y: if a.y > b.y { a.y - d } else { a.y + d },
+            }));
+        }
     }
 }
 
 pub fn solve(lines: Vec<String>) -> (usize, usize) {
-    test_point_intersection();
-
     let vents: Vec<Line> = lines.iter().map(|l| Line::from_str(l).unwrap()).collect();
 
-    (part_one(&vents), part_two(&vents))
-}
-
-fn part_one(vents: &Vec<Line>) -> usize {
-    let vert_or_horiz: Vec<Line> = vents
+    let vert_or_horiz: Vec<&Line> = vents
         .iter()
         .filter(|l| l.a.x == l.b.x || l.a.y == l.b.y)
-        .map(|v| *v)
         .collect();
-
-    let overlaps = get_overlaps(&vert_or_horiz);
-
-    overlaps.iter().filter(|(_, v)| **v > 1).count()
-}
-
-fn part_two(vents: &Vec<Line>) -> usize {
-    let overlaps = get_overlaps(&vents);
-
-    // let mut output = String::new();
-    // for y in 0..10 {
-    //     let mut row = String::new();
-    //     for x in 0..10 {
-    //         if let Some(val) = overlaps.get(&Point { x, y }) {
-    //             row.push_str(format!("{}", val).as_str());
-    //         } else {
-    //             row.push('.');
-    //         }
-    //     }
-    //     output.push_str(row.as_str());
-    //     output.push('\n');
-    // }
-    // println!("{}", output);
-    overlaps.iter().filter(|(_, v)| **v > 1).count()
-}
-
-fn get_overlaps(vents: &Vec<Line>) -> HashMap<Point, u32> {
-    let ((min_x, min_y), (max_x, max_y)) = get_vent_range(vents);
+    let diag: Vec<&Line> = vents
+        .iter()
+        .filter(|l| l.a.x != l.b.x && l.a.y != l.b.y)
+        .collect();
 
     let mut overlaps: HashMap<Point, u32> = HashMap::new();
 
-    for y in min_y..=max_y {
-        for x in min_x..=max_x {
-            let p = Point {
-                x: x as i32,
-                y: y as i32,
-            };
+    add_overlaps(&vert_or_horiz, &mut overlaps);
+    let part_one = overlaps.iter().filter(|(_, v)| **v > 1).count();
 
-            for vent in vents {
-                if vent.intersects(&p) {
-                    let entry = overlaps.entry(p).or_insert(0);
-                    *entry += 1;
-                    // println!("{:?} is in {:?} for {} times", &p, &vent, entry);
-                }
+    add_overlaps(&diag, &mut overlaps);
+    let part_two = overlaps.iter().filter(|(_, v)| **v > 1).count();
+
+    (part_one, part_two)
+}
+
+fn add_overlaps(vents: &Vec<&Line>, overlaps: &mut HashMap<Point, u32>) {
+    for vent in vents {
+        for point in vent.get_points_iter() {
+            let entry = overlaps.entry(point).or_insert(0);
+            if *entry > 2 {
+                continue;
             }
+            *entry += 1;
         }
     }
-
-    overlaps
-}
-
-fn get_vent_range(vents: &Vec<Line>) -> ((usize, usize), (usize, usize)) {
-    let mut xs = Vec::new();
-    let mut ys = Vec::new();
-
-    for vent in vents {
-        xs.push(vent.a.x);
-        xs.push(vent.b.x);
-        ys.push(vent.a.y);
-        ys.push(vent.b.y);
-    }
-
-    (
-        (
-            *xs.iter().min().unwrap() as usize,
-            *ys.iter().min().unwrap() as usize,
-        ),
-        (
-            *xs.iter().max().unwrap() as usize,
-            *ys.iter().max().unwrap() as usize,
-        ),
-    )
-}
-
-fn test_point_intersection() {
-    let line = Line::from_str("0,0 -> 500,500").unwrap();
-    let back_line = Line::from_str("500,500 -> 0,0").unwrap();
-    let short = Line::from_str("100,100 -> 101,101").unwrap();
-    let weird = Line::from_str("433,296 -> 148,581").unwrap();
-
-    assert!(line.intersects(&Point { x: 250, y: 250 }));
-    assert!(back_line.intersects(&Point { x: 250, y: 250 }));
-    assert!(short.intersects(&Point { x: 100, y: 100 }));
-    assert!(short.intersects(&Point { x: 101, y: 101 }));
-    assert!(weird.intersects(&Point { x: 283, y: 446 }));
 }
