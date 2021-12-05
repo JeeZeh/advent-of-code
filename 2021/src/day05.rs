da2bc1b06e788df71b332cad530f47143b973f10
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2021-12-05 17:47:18 +0000

    Discard HashMap

--- 2021/src/day05.rs
+++ 2021/src/day05.rs
@@ -1,90 +1,63 @@
-use std::{
-    borrow::Borrow, collections::HashMap, num::ParseIntError, str::FromStr, string::ParseError,
-};
-
-#[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
-struct Point {
-    x: i32,
-    y: i32,
-}
-
-impl FromStr for Point {
-    type Err = ParseIntError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parts: Vec<&str> = s.split(",").collect();
-        let x = parts[0].parse()?;
-        let y = parts[1].parse()?;
-        Ok(Point { x, y })
-    }
-}
-
 #[derive(Clone, Debug)]
 struct Line {
-    a: Point,
-    b: Point,
-}
-
-impl FromStr for Line {
-    type Err = ParseError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parts: Vec<&str> = s.split(" -> ").collect();
-        let a: Point = Point::from_str(parts[0]).unwrap();
-        let b: Point = Point::from_str(parts[1]).unwrap();
-
-        Ok(Line { a, b })
-    }
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
 }
 
 impl Line {
-    fn get_points_iter(&self) -> Box<dyn Iterator<Item = Point> + '_> {
-        let (a, b) = (self.a.borrow(), self.b.borrow());
+    fn parse(s: &str) -> Self {
+        let points: Vec<&str> = s.split(" -> ").collect();
+        let a: Vec<&str> = points[0].split(",").collect();
+        let b: Vec<&str> = points[1].split(",").collect();
 
-        if a.x == b.x {
-            return Box::new((a.y.min(b.y)..=a.y.max(b.y)).map(|y| Point { x: a.x, y }));
-        } else if a.y == b.y {
-            return Box::new((a.x.min(b.x)..=a.x.max(b.x)).map(|x| Point { x, y: a.y }));
+        Line {
+            x1: a[0].parse().unwrap(),
+            y1: a[1].parse().unwrap(),
+            x2: b[0].parse().unwrap(),
+            y2: b[1].parse().unwrap(),
+        }
+    }
+
+    fn get_points_iter(&self) -> Box<dyn Iterator<Item = (i32, i32)> + '_> {
+        let (x1, y1, x2, y2) = (self.x1, self.y1, self.x2, self.y2);
+
+        if x1 == x2 {
+            return Box::new((y1.min(y2)..=y1.max(y2)).map(move |y| (x1, y)));
+        } else if y1 == y2 {
+            return Box::new((x1.min(x2)..=x1.max(x2)).map(move |x| (x, y1)));
         } else {
-            return Box::new((0..=(a.x - b.x).abs()).map(|d| Point {
-                x: if a.x > b.x { a.x - d } else { a.x + d },
-                y: if a.y > b.y { a.y - d } else { a.y + d },
+            return Box::new((0..=(x1 - x2).abs()).map(move |d| {
+                (
+                    if x1 > x2 { x1 - d } else { x1 + d },
+                    if y1 > y2 { y1 - d } else { y1 + d },
+                )
             }));
         }
     }
 }
 
 pub fn solve(lines: Vec<String>) -> (usize, usize) {
-    let vents: Vec<Line> = lines.iter().map(|l| Line::from_str(l).unwrap()).collect();
+    let vents: Vec<Line> = lines.iter().map(|l| Line::parse(l)).collect();
 
-    let vert_or_horiz: Vec<&Line> = vents
-        .iter()
-        .filter(|l| l.a.x == l.b.x || l.a.y == l.b.y)
-        .collect();
-    let diag: Vec<&Line> = vents
-        .iter()
-        .filter(|l| l.a.x != l.b.x && l.a.y != l.b.y)
-        .collect();
+    let (vert_or_horiz, diag) = vents.iter().partition(|l| l.x1 == l.x2 || l.y1 == l.y2);
 
-    let mut overlaps: HashMap<Point, u32> = HashMap::new();
+    let mut overlaps = [[0; 1000]; 1000];
 
     add_overlaps(&vert_or_horiz, &mut overlaps);
-    let part_one = overlaps.iter().filter(|(_, v)| **v > 1).count();
+    let part_one = overlaps.iter().flatten().filter(|v| **v > 1).count();
 
     add_overlaps(&diag, &mut overlaps);
-    let part_two = overlaps.iter().filter(|(_, v)| **v > 1).count();
+    let part_two = overlaps.iter().flatten().filter(|v| **v > 1).count();
 
     (part_one, part_two)
 }
 
-fn add_overlaps(vents: &Vec<&Line>, overlaps: &mut HashMap<Point, u32>) {
+fn add_overlaps(vents: &Vec<&Line>, overlaps: &mut [[u8; 1000]; 1000]) {
     for vent in vents {
-        for point in vent.get_points_iter() {
-            let entry = overlaps.entry(point).or_insert(0);
-            if *entry > 2 {
-                continue;
-            }
-            *entry += 1;
+        for (x, y) in vent.get_points_iter() {
+            overlaps[y as usize][x as usize] += 1;
         }
     }
 }
