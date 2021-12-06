680fdbde43345c3f20733fb13611bed34c9d46e8
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2021-12-06 21:50:06 +0000

    2021:06 finally

--- 2021/src/day06.rs
+++ 2021/src/day06.rs
@@ -1,21 +1,33 @@
-pub fn solve(line: String) -> (usize, usize) {
-    let starting: Vec<i32> = line.split(",").map(|c| c.parse().unwrap()).collect();
+pub fn solve(line: String) -> (u64, u64) {
+    let mut fish: [u64; 9] = [0; 9];
 
-    let mut total = 0;
+    line.split(",")
+        .map(|c| c.parse::<usize>().unwrap())
+        .for_each(|n| fish[n] += 1);
 
-    for fish in starting {
-        total += get_new_fish_count(fish, 800);
-    }
+    do_step(&mut fish, 80);
+    let part_one = fish.iter().sum();
+    do_step(&mut fish, 256 - 80);
+    let part_two = fish.iter().sum();
 
-    (total as usize, 0)
+    (part_one, part_two)
 }
 
-fn get_new_fish_count(fish: i32, remaining: i32) -> i32 {
-    let mut total = 1;
+fn do_step(fish: &mut [u64; 9], n: usize) {
+    // This works by holding the count of each fish at a given
+    // internal counter in an array of 'bins' where fish[1] is the number
+    // of fish with an internal counter of 1
 
-    for rem in (0..remaining - fish).rev().step_by(7) {
-        total += get_new_fish_count(8, rem);
+    // Every day that passes, all fish have their counters decremented by
+    // rotating the array to the left (fish at counter 1 become counter 0, 0 become 8, etc.).
+
+    // The fish now at counter 8 represent both the new fish (they're left at 8) and their
+    // parents which should actually reset to counter 6, so they're copied to index 6 of the array.
+    for _ in 0..n {
+        let to_add = fish[0];
+        fish.rotate_left(1);
+        fish[6] += fish[8];
+        fish[8] = to_add;
     }
-
-    total
 }
+
