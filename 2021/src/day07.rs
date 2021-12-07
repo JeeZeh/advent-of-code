6582458ef3a98ecc50f805b0e102b514a984324b
Jesse Ashmore <ashmjss@amazon.com>
2021-12-07 15:07:44 +0000

    Crazy optimisation for part 2

--- 2021/src/day07.rs
+++ 2021/src/day07.rs
@@ -10,12 +10,12 @@ pub fn solve(line: String) -> (i32, i32) {
         .min()
         .unwrap();
 
-    let part_two_fuel: i32 = (*bins.keys().min().unwrap()..*bins.keys().max().unwrap())
-        .map(|pos| get_fuel_for_pos(&bins, pos, true))
-        .min()
-        .unwrap();
+    let part_two_optimal_position = get_mean(&crab_positions);
 
-    (part_one_fuel, part_two_fuel)
+    (
+        part_one_fuel,
+        get_fuel_for_pos(&bins, part_two_optimal_position, true),
+    )
 }
 
 fn get_fuel_for_pos(crab_bins: &HashMap<i32, i32>, pos: i32, sequence: bool) -> i32 {
@@ -41,4 +41,11 @@ fn build_crab_bins(crabs: &Vec<i32>) -> HashMap<i32, i32> {
     }
 
     bins
+}
+
+// Optimal position for Part 2 is literally just the mean...
+// https://www.reddit.com/r/adventofcode/comments/rawxad/2021_day_7_part_2_i_wrote_a_paper_on_todays/
+fn get_mean(crabs: &Vec<i32>) -> i32 {
+    let mean = crabs.iter().sum::<i32>() / crabs.len() as i32;
+    mean
 }
