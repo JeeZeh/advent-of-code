356ef04fdc34868d35e41b0cac6bbfe8750ca71e
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2021-12-12 21:31:34 +0000

    Speedup

--- 2021/src/day12.rs
+++ 2021/src/day12.rs
@@ -1,7 +1,4 @@
-use std::{
-    borrow::Borrow,
-    collections::{HashMap, HashSet, VecDeque},
-};
+use std::collections::{HashMap, HashSet};
 
 use itertools::Itertools;
 
@@ -12,64 +9,113 @@ use itertools::Itertools;
 //     connects: Vec<Cave>,
 // }
 
-pub fn solve(lines: Vec<String>) -> (usize, usize) {
+pub fn solve(lines: Vec<String>) -> (u32, u32) {
     let connections = parse_connections(&lines);
-    let all_paths = explore(&connections);
-    (all_paths.len(), all_paths.last().unwrap().len())
+    let mut seen = HashSet::new();
+    seen.insert(-1);
+    let all_paths = explore_rec(&connections, &mut seen, -1, false);
+    (all_paths, 0)
 }
 
-fn explore(connections: &HashMap<String, HashSet<String>>) -> Vec<Vec<String>> {
-    let mut paths: Vec<Vec<String>> = Vec::new();
-    let mut seen: HashSet<String> = HashSet::new();
-    seen.insert(String::from("start"));
+fn explore_rec(
+    connections: &HashMap<i8, Vec<i8>>,
+    seen: &mut HashSet<i8>,
+    current: i8,
+    visited_twice: bool,
+) -> u32 {
+    let mut paths = 0;
+    // dbg!(&seen, current, visited_twice);
 
-    let mut queue: VecDeque<(Vec<String>, HashSet<String>, bool)> = VecDeque::new();
-    queue.push_back((vec![String::from("start")], seen, false));
-
-    while let Some((path, seen, visited_twice)) = queue.pop_front() {
-        let current = path.last().unwrap();
-
-        if current == "end" {
-            paths.push(path);
-        } else {
-            for connects in connections.get(current).unwrap() {
-                let visited_before = seen.contains(connects);
-                if !visited_before || (!visited_twice && connects != "start" && connects != "end") {
-                    let mut path = path.clone();
-                    let mut seen = seen.clone();
-                    if &connects.to_ascii_lowercase() == connects {
-                        seen.insert(connects.to_string());
-                    }
-                    path.push(connects.to_string());
-                    queue.push_back((path, seen, visited_before || visited_twice));
+    if current == -2 {
+        paths += &1;
+    } else {
+        for connects in connections.get(&current).unwrap() {
+            let visited_before = seen.contains(connects);
+            // println!("{} ? {} ({})", current, connects, visited_before);
+            if !visited_before || (!visited_twice && *connects != -1 && *connects != -1) {
+                let mut seen = seen.clone();
+                if *connects < 0 {
+                    seen.insert(*connects);
+                }
+                paths += explore_rec(
+                    connections,
+                    &mut seen,
+                    *connects,
+                    visited_before || visited_twice,
+                );
+                if *connects < 0 {
+                    seen.remove(&connects);
                 }
             }
         }
     }
 
-    // dbg!(paths.iter().map(|p| p.join(",")).sorted());
-
-    paths
+    return paths;
 }
 
-fn parse_connections(lines: &[String]) -> HashMap<String, HashSet<String>> {
-    let mut connections: HashMap<String, HashSet<String>> = HashMap::new();
+// fn explore(connections: &HashMap<i8, Vec<i8>>) -> Vec<Vec<String>> {
+//     let mut paths: Vec<Vec<String>> = Vec::new();
+//     let mut seen: HashSet<String> = HashSet::new();
+//     seen.insert(String::from("start"));
+
+//     let mut queue: VecDeque<(Vec<String>, HashSet<String>, bool)> = VecDeque::new();
+//     queue.push_back((vec![String::from("start")], seen, false));
+
+//     while let Some((path, seen, visited_twice)) = queue.pop_front() {
+//         let current = path.last().unwrap();
+
+//         if current == "end" {
+//             paths.push(path);
+//         } else {
+//             for connects in connections.get(current).unwrap() {
+//                 let visited_before = seen.contains(connects);
+//                 if !visited_before || (!visited_twice && connects != "start" && connects != "end") {
+//                     let mut path = path.clone();
+//                     let mut seen = seen.clone();
+//                     if &connects.to_ascii_lowercase() == connects {
+//                         seen.insert(connects.to_string());
+//                     }
+//                     path.push(connects.to_string());
+//                     queue.push_back((path, seen, visited_before || visited_twice));
+//                 }
+//             }
+//         }
+//     }
+
+//     // dbg!(paths.iter().map(|p| p.join(",")).sorted());
+
+//     paths
+// }
+
+fn parse_connections(lines: &[String]) -> HashMap<i8, Vec<i8>> {
+    let mut connections: HashMap<i8, Vec<i8>> = HashMap::new();
+    let mut ids: HashMap<String, i8> = HashMap::new();
+    ids.insert(String::from("start"), -1);
+    ids.insert(String::from("end"), -2);
     // let mut caves: HashMap<String, Cave> = HashMap::new();
+
+    for cave in lines
+        .iter()
+        .map(|l| l.split('-').map(String::from).collect::<Vec<String>>())
+        .flatten()
+        .unique()
+    {
+        if !ids.contains_key(&cave) {
+            ids.insert(
+                String::from(&cave),
+                (ids.len() + 1) as i8 * if cave == cave.to_lowercase() { -1 } else { 1 },
+            );
+        }
+    }
 
     for line in lines {
         let mut parts = line.split('-');
-        let from_id = String::from(parts.next().unwrap());
-        let to_id = String::from(parts.next().unwrap());
+        let from = ids.get(parts.next().unwrap()).unwrap();
+        let to = ids.get(parts.next().unwrap()).unwrap();
 
-        connections
-            .entry(from_id.to_string())
-            .or_insert(HashSet::new())
-            .insert(to_id.to_string());
+        connections.entry(*from).or_insert(Vec::new()).push(*to);
 
-        connections
-            .entry(to_id.to_string())
-            .or_insert(HashSet::new())
-            .insert(from_id.to_string());
+        connections.entry(*to).or_insert(Vec::new()).push(*from);
     }
 
     // for (entry, _) in connections {
