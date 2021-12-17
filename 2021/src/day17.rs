c216c36686bdb2609a2f0d238aa553e5fc86b1d3
Jesse Ashmore <jesse.ashmore97@gmail.com>
2021-12-17 18:15:29 +0000

    Add some neat constraints on part 1 and 2

--- 2021/src/day17.rs
+++ 2021/src/day17.rs
@@ -1,14 +1,10 @@
-use std::{collections::HashSet, ops::Range};
-
-use itertools::Itertools;
-
+use std::ops::Range;
 #[derive(Debug)]
 struct Drone {
     pos_x: i32,
     pos_y: i32,
     vel_x: i32,
     vel_y: i32,
-    max_y: i32,
 }
 
 impl Drone {
@@ -18,7 +14,6 @@ impl Drone {
             pos_y: 0,
             vel_x,
             vel_y,
-            max_y: 0,
         }
     }
 
@@ -31,9 +26,6 @@ impl Drone {
         self.vel_x = 0.max(self.vel_x + (if self.vel_x < 0 { 1 } else { -1 }));
         // Due to gravity, the probe's y velocity decreases by 1.
         self.vel_y -= 1;
-        if self.pos_y > self.max_y {
-            self.max_y = self.pos_y;
-        }
     }
 }
 
@@ -44,61 +36,47 @@ pub fn solve(string: String) -> (i32, i32) {
 }
 
 fn find_all_vels(target_x: &Range<i32>, target_y: &Range<i32>) -> (i32, i32) {
-    let mut all_vels: Vec<(i32, i32, i32)> = Vec::new();
+    let mut all_vels: Vec<(i32, i32)> = Vec::new();
 
-    for vx in 0..500 {
-        for vy in -300..250 {
-            let (success, drone) = fire(vx, vy, target_x, target_y);
-            if success {
-                all_vels.push((vx, vy, drone.max_y));
+    let min_x = (-1 + (1.00 + 8.00 * target_x.start as f32).sqrt() as i32) / 2;
+
+    for vx in min_x..target_x.end {
+        let vy_range = if vx > target_x.start {
+            // Use this bound if we're immediately landing in the target (y vel must be inside the target)
+            target_y.start..target_y.end
+        } else {
+            // I'm sure you can bound this more, given the number of steps it takes to reach the target
+            // in the x direction you should be able to determine at least the minimum bound
+            target_y.start..target_y.start.abs()
+        };
+        for vy in vy_range {
+            if fire(vx, vy, target_x, target_y) {
+                all_vels.push((vx, vy));
             }
         }
     }
 
-    // dbg!(&all_vels);
     (
-        all_vels.iter().map(|(_, _, h)| *h).max().unwrap(),
+        target_y.start.abs() * (target_y.start.abs() - 1) / 2,
         all_vels.len() as i32,
     )
 }
-fn maximise_vy(target_x: &Range<i32>, target_y: &Range<i32>) -> i32 {
-    let mut vx = 0;
-    let mut vy = 0;
 
-    let mut max_height = i32::MIN;
-
-    while vy < 150 {
-        let (success, drone) = fire(vx, vy, target_x, target_y);
-        if success {
-            max_height = drone.max_y;
-        }
-
-        if drone.pos_x < target_x.start {
-            vx += 1;
-        } else if drone.pos_x > target_x.end {
-            vx -= 1;
-        } else {
-            vy += 1;
-        }
-    }
-
-    max_height
-}
-
-fn fire(vel_x: i32, vel_y: i32, target_x: &Range<i32>, target_y: &Range<i32>) -> (bool, Drone) {
+fn fire(vel_x: i32, vel_y: i32, target_x: &Range<i32>, target_y: &Range<i32>) -> bool {
     let mut drone = Drone::with_vel(vel_x, vel_y);
 
-    while !(drone.pos_x < target_x.start && drone.vel_x == 0) && !(drone.pos_y < target_y.start) {
+    while !(drone.pos_x < target_x.start && drone.vel_x == 0)
+        && !(drone.pos_y < target_y.start)
+        && !(drone.pos_x > target_x.end)
+    {
         if target_x.contains(&drone.pos_x) && target_y.contains(&drone.pos_y) {
-            return (true, drone);
+            return true;
         }
-
-        // dbg!(&drone);
 
         drone.step();
     }
 
-    return (false, drone);
+    return false;
 }
 
 fn parse_targets(string: String) -> (Range<i32>, Range<i32>) {
