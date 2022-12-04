d55530e9ebe05dde99aa4bed0f58994962415789
Jesse Ashmore <jesse.ashmore97@gmail.com>
2022-12-04 16:41:42 +0000

    refactor(2022-04): use Zone in place of Range

--- 2022/src/day04.rs
+++ 2022/src/day04.rs
@@ -1,6 +1,8 @@
-use std::ops::RangeInclusive;
-
-struct Pair(RangeInclusive<u32>, RangeInclusive<u32>);
+struct Pair(Zone, Zone);
+struct Zone {
+    start: u32,
+    end: u32,
+}
 
 pub fn solve(lines: Vec<String>) -> (usize, usize) {
     let pairs: Vec<Pair> = lines.iter().map(|s| parse_pair(s)).collect();
@@ -12,25 +14,25 @@ pub fn solve(lines: Vec<String>) -> (usize, usize) {
 
 impl Pair {
     fn overlaps_fully(&self) -> bool {
-        let left_overlaps_right = self.0.start() <= self.1.start() && self.0.end() >= self.1.end();
-        let right_overlaps_left = self.1.start() <= self.0.start() && self.1.end() >= self.0.end();
+        let left_overlaps_right = self.0.start <= self.1.start && self.0.end >= self.1.end;
+        let right_overlaps_left = self.1.start <= self.0.start && self.1.end >= self.0.end;
         left_overlaps_right || right_overlaps_left
     }
     fn overlaps_partially(&self) -> bool {
-        self.0.start() <= self.1.end() && self.0.end() >= self.1.start()
+        self.0.start <= self.1.end && self.0.end >= self.1.start
     }
 }
 
 fn parse_pair(s: &str) -> Pair {
     let mut pair_parts = s.split(",");
-    let left = parse_range(pair_parts.next().unwrap());
-    let right = parse_range(pair_parts.next().unwrap());
-    Pair(left, right)
+    let left = parse_zone(pair_parts.next().unwrap());
+    let right = parse_zone(pair_parts.next().unwrap());
+    return Pair(left, right);
 }
 
-fn parse_range(s: &str) -> RangeInclusive<u32> {
+fn parse_zone(s: &str) -> Zone {
     let mut parts = s.split("-");
     let start = parts.next().unwrap().parse().unwrap();
     let end = parts.next().unwrap().parse().unwrap();
-    start..=end
+    return Zone { start, end };
 }
