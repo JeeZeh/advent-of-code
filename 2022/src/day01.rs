5134f22b622bdd71a1b2c0fd71f00785e88298e7
Jesse Ashmore <jesse.ashmore97@gmail.com>
2022-12-08 00:35:14 +0000

    refactor(2022-01): remove unnecessary cleaning of \r\n

--- 2022/src/day01.rs
+++ 2022/src/day01.rs
@@ -2,7 +2,6 @@ use itertools::Itertools;
 
 pub fn solve(input: String) -> (usize, usize) {
     let elves: Vec<usize> = input
-        .replace("\r\n", "\n")
         .split("\n\n")
         .map(|elf| elf.lines().map(|c| c.parse::<usize>().unwrap()).sum())
         .sorted()
