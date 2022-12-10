629b447ac350535b89bbe63752bdfe63f6a59ce9
Jesse Ashmore <jesse.ashmore97@gmail.com>
2022-12-10 02:09:52 +0000

    refactor(2022-09): move definitions around, add comments

--- 2022/src/day09.rs
+++ 2022/src/day09.rs
@@ -5,19 +5,64 @@ use std::{
 
 use itertools::Itertools;
 
-#[derive(Clone, Copy, PartialEq, Eq, Debug, Hash)]
-struct Pos(i32, i32);
+pub fn solve(input: String) -> (usize, usize) {
+    let instructions = input.lines().map(Instruction::from).collect_vec();
 
-impl From<&Direction> for Pos {
-    fn from(dir: &Direction) -> Self {
-        match dir {
-            Direction::Up => Pos(0, 1),
-            Direction::Down => Pos(0, -1),
-            Direction::Left => Pos(-1, 0),
-            Direction::Right => Pos(1, 0),
+    (
+        simulate(&instructions, &mut vec![Pos(0, 0); 2]),
+        simulate(&instructions, &mut vec![Pos(0, 0); 10]),
+    )
+}
+
+// Apply a list of instructions to a chain of entities, returning the number of unique
+// positions visited by the tail
+fn simulate(instructions: &[Instruction], entities: &mut Vec<Pos>) -> usize {
+    let mut visited: HashSet<Pos> = HashSet::new();
+    visited.insert(Pos(0, 0));
+    instructions
+        .iter()
+        .for_each(|Instruction(dir, steps)| move_entities(&mut visited, entities, dir, *steps));
+    visited.len()
+}
+
+// Move the head in a direction a certain number of steps, and
+// propagate movement down the chain of entities.
+fn move_entities(
+    visited: &mut HashSet<Pos>,
+    entities: &mut Vec<Pos>,
+    direction: &Direction,
+    steps: usize,
+) {
+    let len = entities.len();
+    for _ in 0..steps {
+        for i in 0..len {
+            if i == 0 {
+                let target = entities.get_mut(i).unwrap();
+                *target += direction.into();
+            } else {
+                let target = *entities.get(i - 1).unwrap();
+                let entity = entities.get_mut(i).unwrap();
+                entity.follow(&target);
+                // Only track the tail position in visited list
+                if i == len - 1 {
+                    visited.insert(*entity);
+                }
+            }
         }
     }
 }
+
+// Types //
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Hash)]
+struct Pos(i32, i32);
 
 impl Pos {
     fn follow(&mut self, target: &Pos) {
@@ -31,7 +76,7 @@ impl Pos {
             return;
         }
         // Offset by 1 horizontally or vertically is okay
-        if (rel_x.abs() == 1 && rel_y == 0) || (rel_y.abs() == 1 && rel_x == 0) {
+        if rel_x.abs() + rel_y == 1 {
             return;
         }
 
@@ -61,11 +106,15 @@ impl AddAssign for Pos {
     }
 }
 
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
+impl From<&Direction> for Pos {
+    fn from(dir: &Direction) -> Self {
+        match dir {
+            Direction::Up => Pos(0, 1),
+            Direction::Down => Pos(0, -1),
+            Direction::Left => Pos(-1, 0),
+            Direction::Right => Pos(1, 0),
+        }
+    }
 }
 
 struct Instruction(Direction, usize);
@@ -83,48 +132,5 @@ impl From<&str> for Instruction {
         let steps = right.parse().unwrap();
 
         Instruction(direction, steps)
-    }
-}
-
-pub fn solve(input: String) -> (usize, usize) {
-    let instructions = input.lines().map(Instruction::from).collect_vec();
-
-    (
-        simulate(&instructions, &mut vec![Pos(0, 0); 2]),
-        simulate(&instructions, &mut vec![Pos(0, 0); 10]),
-    )
-}
-
-fn simulate(instructions: &[Instruction], entities: &mut Vec<Pos>) -> usize {
-    let mut part_one_visited: HashSet<Pos> = HashSet::new();
-    part_one_visited.insert(Pos(0, 0));
-    instructions.iter().for_each(|Instruction(dir, steps)| {
-        move_entities(&mut part_one_visited, entities, dir, *steps)
-    });
-    part_one_visited.len()
-}
-
-fn move_entities(
-    visited: &mut HashSet<Pos>,
-    entities: &mut Vec<Pos>,
-    direction: &Direction,
-    steps: usize,
-) {
-    let len = entities.len();
-    for _ in 0..steps {
-        for i in 0..len {
-            if i == 0 {
-                let target = entities.get_mut(i).unwrap();
-                *target += direction.into();
-            } else {
-                let target = *entities.get(i - 1).unwrap();
-                let entity = entities.get_mut(i).unwrap();
-                entity.follow(&target);
-                // Only track the tail position in visited list
-                if i == len - 1 {
-                    visited.insert(*entity);
-                }
-            }
-        }
     }
 }
