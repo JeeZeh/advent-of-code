957ffb10fd5072d2f38ed70ca9184572d8138d78
Jesse Ashmore <jesse.ashmore97@gmail.com>
2022-12-14 01:22:48 +0000

    solve(2022-13): 🎄but on fire - worst solution I've ever written, what a mess

--- 2022/src/day13.rs
+++ 2022/src/day13.rs
@@ -6,15 +6,46 @@ pub fn solve(input: String) -> (usize, usize) {
         .map(|p| p.split_once("\n").unwrap())
         .collect_vec();
 
-    (
-        packets
-            .iter()
-            .enumerate()
-            .filter(|(_, (l, r))| is_sorted(l, r).unwrap())
-            .map(|(i, _)| i + 1)
-            .sum(),
-        0,
-    )
+    let part_one = packets
+        .iter()
+        .enumerate()
+        .filter(|(_, (l, r))| is_sorted(l, r).unwrap())
+        .map(|(i, _)| i + 1)
+        .sum();
+
+    (part_one, part_two(input))
+}
+
+fn part_two(input: String) -> usize {
+    let joined_input = input.replace("\n\n", "\n");
+    let mut augmented_input = joined_input.lines().collect_vec();
+    augmented_input.push("[[2]]");
+    augmented_input.push("[[6]]");
+    let all_sorted = sort_all(&augmented_input);
+    let (div_one, _) = all_sorted
+        .iter()
+        .enumerate()
+        .find(|(i, p)| *p == "[[2]]")
+        .unwrap();
+    let (div_two, _) = all_sorted
+        .iter()
+        .enumerate()
+        .find(|(i, p)| *p == "[[6]]")
+        .unwrap();
+
+    (div_one + 1) * (div_two + 1)
+}
+
+fn sort_all(input: &[&str]) -> Vec<String> {
+    input
+        .iter()
+        .map(|s| s.to_string())
+        .sorted_by(|a, b| match is_sorted(a, b) {
+            Some(true) => std::cmp::Ordering::Less,
+            Some(false) => std::cmp::Ordering::Greater,
+            None => std::cmp::Ordering::Equal,
+        })
+        .collect_vec()
 }
 
 fn is_sorted(left: &str, right: &str) -> Option<bool> {
@@ -29,11 +60,8 @@ fn is_sorted(left: &str, right: &str) -> Option<bool> {
     }
 
     if left.is_list() && right.is_list() {
-        if left == "[]" {
-            return Some(true);
-        }
-        if right == "[]" {
-            return Some(false);
+        if left == "[]" && right == "[]" {
+            return None;
         }
         for next in left
             .get_list_slices()
@@ -90,10 +118,12 @@ impl<'a> PacketStr<&'a str> for &str {
     fn next_value_slice(&self, start: usize) -> Option<&str> {
         let mut start_pos = None;
         let mut end_pos = None;
+        let mut expect_list = false;
         let mut relative_nesting = 0;
         for (offset, c) in self.chars().skip(start).enumerate() {
             if start_pos.is_none() && c != ']' && c != ',' {
                 start_pos = Some(start + offset);
+                expect_list = c == '[';
             }
             if c == '[' {
                 relative_nesting += 1;
@@ -103,19 +133,18 @@ impl<'a> PacketStr<&'a str> for &str {
             }
             if start_pos.is_some()
                 && end_pos.is_none()
-                && c != '['
-                && c != ','
-                && relative_nesting == 0
+                && relative_nesting <= 0
+                && (c == ']' || c == ',')
             {
-                end_pos = Some(start + offset)
+                if expect_list {
+                    end_pos = Some(start + offset)
+                } else {
+                    end_pos = Some(start + offset - 1)
+                }
             }
             if start_pos.is_some() && end_pos.is_some() {
-                break;
+                return Some(&self[start_pos.unwrap()..=end_pos.unwrap()]);
             }
-        }
-
-        if start_pos.is_some() && end_pos.is_some() {
-            return Some(&self[start_pos.unwrap()..=end_pos.unwrap()]);
         }
         None
     }
@@ -135,6 +164,12 @@ mod tests {
     fn next_value_list() {
         let input = "[1,2,3]";
         assert_eq!(input.next_value_slice(0).unwrap(), "[1,2,3]");
+    }
+
+    #[test]
+    fn next_value_long_list() {
+        let input = "[10,2,3]";
+        assert_eq!(input.next_value_slice(1).unwrap(), "10");
     }
 
     #[test]
@@ -191,11 +226,38 @@ mod tests {
     }
 
     #[test]
+    fn is_sorted_list_not_helpful() {
+        assert_eq!(is_sorted("[[1,1,1],2]", "[[1,1,1],3]").unwrap(), true);
+        assert_eq!(is_sorted("[[1,1,1],2]", "[[1,1,1],1]").unwrap(), false);
+    }
+
+    #[test]
     fn is_sorted_all_test_cases() {
         assert_eq!(is_sorted("[1,1,3,1,1]", "[1,1,5,1,1]").unwrap(), true);
         assert_eq!(is_sorted("[[1],[2,3,4]]", "[[1],4]").unwrap(), true);
         assert_eq!(is_sorted("[9]", "[[8,7,6]]").unwrap(), false);
         assert_eq!(is_sorted("[[4,4],4,4]", "[[4,4],4,4,4]").unwrap(), true);
         assert_eq!(is_sorted("[7,7,7,7]", "[7,7,7]").unwrap(), false);
+    }
+
+    #[test]
+    fn real_8() {
+        let left = "[[10,[]]]";
+        let right = "[[[4],[3,4,2]],[3,[[],[6,5],6,4,[8,4,5]],[],[3,[],4,[0]],3],[7,[[0]],[[],[5,9,5],4,[2,3]]]]";
+        assert_eq!(is_sorted(left, right).unwrap(), false);
+    }
+
+    #[test]
+    fn real_9() {
+        let left = "[[10,2,4],[],[]]";
+        let right = "[[4,[7,0,9]]]";
+        assert_eq!(is_sorted(left, right).unwrap(), false);
+    }
+
+    #[test]
+    fn real_85() {
+        let left = "[[],[8],[8,3],[]]";
+        let right = "[[],[2,[4,3,6]],[8]]";
+        assert_eq!(is_sorted(left, right).unwrap(), false);
     }
 }
