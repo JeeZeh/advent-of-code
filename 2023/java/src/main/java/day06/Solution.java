60bcc5d7aad084a8d4fa0a747061e8a5d2a072dc
Jesse Ashmore <jesse.ashmore97@gmail.com>
2023-12-06 23:00:38 +0000

    aoc(2023-06): solve 🎄

--- 2023/java/src/main/java/day06/Solution.java
+++ 2023/java/src/main/java/day06/Solution.java
@@ -1,53 +1,62 @@
 package day06;
 
 import static java.lang.StringTemplate.STR;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.IntStream;
 import java.util.stream.LongStream;
+
 import com.google.common.collect.Lists;
 import lib.Input;
 
 public class Solution {
-  List<String> lines = Input.lines("day06/example.txt").toList();
+
+  public static void main(String[] args) throws IOException {
+    List<String> lines = Input.lines("day06/input.txt").toList();
+
+    List<List<String>> splits = lines.stream().map(line -> Arrays.stream(line.split("\\s+")).skip(1).toList()).toList();
+
+    StringBuilder superTime = new StringBuilder();
+    StringBuilder superDistance = new StringBuilder();
+
+    List<Race> races = new ArrayList<>();
+    for (int i = 0; i < splits.get(0).size(); i++) {
+      String time = splits.get(0).get(i);
+      String dist = splits.get(1).get(i);
+      superTime.append(time);
+      superDistance.append(dist);
+      races.add(new Race(Long.parseLong(time), Long.parseLong(dist)));
+    }
+    Race superRace = new Race(Long.parseLong(superTime.toString()), Long.parseLong(superDistance.toString()));
+
+    long partOne = races.stream().mapToLong(race -> race.getStrategies().stream().filter(strat -> strat.beats).count()).reduce(1, (x, y) -> x * y);
+    long partTwo = superRace.getStrategies().stream().filter(strat -> strat.beats).count();
+
+    System.out.println(STR. "Part 1: \{ partOne }" );
+    System.out.println(STR. "Part 2: \{ partTwo }" );
+  }
 
 
-  public record Race(int time, int record) {
+  public record Race(long time, long record) {
 
-    public record Strategy(int holdTime, boolean beats) {
+    public record Strategy(long holdTime, boolean beats) {
     }
 
-    int getDistance(int holdTime) {
+    long getDistance(long holdTime) {
       return (time - holdTime) * holdTime;
     }
 
-    boolean beats(int result) {
+    boolean beats(long result) {
       return result > record;
     }
 
     List<Strategy> getStrategies() {
-      return IntStream.range(0, time)
-          .mapToObj(hold -> new Strategy(hold, beats(getDistance(hold))))
-          .toList();
+      return LongStream.range(0, time).mapToObj(hold -> new Strategy(hold, beats(getDistance(hold)))).toList();
     }
   }
 
-  public Solution() throws IOException {
-    List<List<String>> splits =
-        lines.stream().map(line -> Arrays.stream(line.split("\\s+")).skip(1).toList()).toList();
 
-    List<Race> races = new ArrayList<>();
-    for (int i = 0; i < splits.get(0).size(); i++) {
-      races.add(new Race(
-          Integer.parseInt(splits.get(0).get(i)),
-          Integer.parseInt(splits.get(1).get(i))));
-    }
-
-    int partOne = races.stream()
-        .mapToInt(race -> (int) race.getStrategies().stream().filter(strat -> strat.beats).count())
-        .reduce(1, (x, y) -> x * y);
-  }
 }
