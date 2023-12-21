c5369d00f2519fc268b3916d2927ae88181e631a
Jesse Ashmore <jesse.ashmore97@gmail.com>
2023-12-21 21:00:22 +0000

    aoc(2023-21): solve with hints

--- 2023/java/src/main/java/day21/Solution.java
+++ 2023/java/src/main/java/day21/Solution.java
@@ -21,15 +21,48 @@ public class Solution {
     List<String> lines = Input.lines("day21/input.txt").toList();
     final Garden garden = Garden.fromLines(lines);
 
-    long partOne = garden.getReachable(garden.start, 64).getOrDefault(64, new HashSet<>()).size();
-    long partTwo = 0;
+    long partOne = garden.getReachable(64).getOrDefault(64, new HashSet<>()).size();
+    long partTwo = getPartTwo(garden, 26501365);
+
     System.out.println(STR."Part 1: \{partOne}");
     System.out.println(STR."Part 2: \{partTwo}");
   }
 
+  private static long getPartTwo(Garden garden, int goal) {
+    // https://www.reddit.com/r/adventofcode/comments/18nevo3/2023_day_21_solutions/keaiiq7/?context=3
+    var maxSteps = garden.start.x() + (garden.width() * 2L);
+    Garden expanded = garden.expand();
+    Map<Integer, Set<Pos>> maxSpaces = expanded.getReachable(maxSteps);
+
+    var a0 = maxSpaces.get(garden.start.x()).size();
+    var a1 = maxSpaces.get(garden.start.x() + (garden.width())).size();
+    var a2 = maxSpaces.get(garden.start.x() + garden.width() * 2).size();
+    var n = goal / garden.width();
+
+    var b0 = a0;
+    var b1 = a1 - a0;
+    var b2 = a2 - a1;
+    return b0 + (long) b1 * n + ((long) n * (n - 1) / 2) * (b2 - b1);
+  }
+
   public record Garden(List<List<Boolean>> elements, Pos start) implements Grid<Boolean> {
 
-    public Map<Integer, Set<Pos>> getReachable(Pos start, long maxSteps) {
+    public Garden expand() {
+      List<List<Boolean>> rows = new ArrayList<>();
+
+      for (int i = 0; i < 5; i++) {
+        for (int y = 0; y < height(); y++) {
+          List<Boolean> row = new ArrayList<>();
+          for (int i1 = 0; i1 < 5; i1++) {
+            row.addAll(this.elements.get(y));
+          }
+          rows.add(row);
+        }
+      }
+      return new Garden(rows, new Pos(rows.size() / 2, rows.getFirst().size() / 2));
+    }
+
+    public Map<Integer, Set<Pos>> getReachable(long maxSteps) {
       Deque<Pair<Pos, Integer>> nextSteps = new ArrayDeque<>();
       Map<Integer, Set<Pos>> reachable = new HashMap<>();
 
