4ed51ed865ace1c818dc7e7eca9142cf03981802
Jesse Ashmore <jesse.ashmore97@gmail.com>
2023-12-24 16:38:20 +0000

    aoc(2023-24): solve 🎄

--- 2023/java/src/main/java/day24/Solution.java
+++ 2023/java/src/main/java/day24/Solution.java
@@ -1,11 +1,18 @@
 package day24;
 
+import com.microsoft.z3.BoolExpr;
+import com.microsoft.z3.Context;
+import com.microsoft.z3.Expr;
+import com.microsoft.z3.RealExpr;
+import com.microsoft.z3.Solver;
+import com.microsoft.z3.Status;
 import java.awt.geom.Point2D;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 import lib.Input;
 
 public class Solution {
@@ -71,6 +78,62 @@ public class Solution {
         .count();
   }
 
+  /**
+   * For each hailstone, we create a constraint such that after some time the position of the thrown
+   * icicle must match the position of the hailstone at that time. Once all of these are expressed,
+   * Z3 will do the work to find the x, y, and z components for us!
+   * <p>
+   * <a
+   * href="https://www.reddit.com/r/adventofcode/comments/18pnycy/2023_day_24_solutions/keq6h44/">Credit
+   * u/keriati</a>
+   */
+  static long partTwo(List<Hail> hails) {
+    try (Context context = new Context()) {
+      final var x = context.mkRealConst("x");
+      final var y = context.mkRealConst("y");
+      final var z = context.mkRealConst("z");
+
+      final var vx = context.mkRealConst("vx");
+      final var vy = context.mkRealConst("vy");
+      final var vz = context.mkRealConst("vz");
+
+      final var solver = context.mkSolver();
+
+      for (int i = 0; i < hails.size(); i++) {
+        var hail = hails.get(i);
+        var time = context.mkRealConst(STR."t\{i}");
+
+        // Only look into the future
+        solver.add(context.mkGe(time, context.mkReal(0)));
+
+        // We must hit this hailstone
+        solver.add(constrainPosition(context, x, vx, hail.x, hail.vx, time));
+        solver.add(constrainPosition(context, y, vy, hail.y, hail.vy, time));
+        solver.add(constrainPosition(context, z, vz, hail.z, hail.vz, time));
+      }
+
+      // Once all constraints are added to the solver, validate that it is solvable.
+      var status = solver.check();
+
+      if (status != Status.SATISFIABLE) {
+        return -1;
+      }
+
+      // Finally, grab the solution for the throw x, y, and z components.
+      var model = solver.getModel();
+
+      return Stream.of(x, y, z).map(expr -> model.eval(expr, true)).map(Expr::toString)
+          .mapToLong(Long::parseLong).sum();
+    }
+  }
+
+  private static BoolExpr constrainPosition(Context context, RealExpr fromX, RealExpr fromVelX,
+      long toX, long toVelX, RealExpr time) {
+    var throwX = context.mkAdd(fromX, context.mkMul(fromVelX, time));
+    var hailX = context.mkAdd(context.mkMul(time, context.mkReal(toVelX)), context.mkReal(toX));
+    return context.mkEq(hailX, throwX);
+  }
+
   public static void main(String[] args) throws IOException {
     boolean example = false;
     List<Hail> hail = Input.lines(example ? "day24/example.txt" : "day24/input.txt")
@@ -80,8 +143,7 @@ public class Solution {
     long to = example ? 27 : 400000000000000L;
 
     System.out.println(STR."Part 1: \{partOne(hail, from, to)}");
-    long partTwo = 0;
-    System.out.println(STR."Part 2: \{partTwo}");
+    System.out.println(STR."Part 2: \{partTwo(hail)}");
   }
 
 
