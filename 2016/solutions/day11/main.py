68dbf0904e8e907610f86987d606bb4d978dd4ad
Jesse Ashmore <jesse.ashmore97@gmail.com>
2024-05-04 19:53:53 +0100

    aoc(2016-11): part 1

--- 2016/solutions/day11/main.py
+++ 2016/solutions/day11/main.py
@@ -2,9 +2,9 @@ from queue import PriorityQueue
 import re
 from collections import deque
 from copy import deepcopy
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from itertools import chain, combinations
-from typing import Literal, NamedTuple, Optional
+from typing import Any, Literal, NamedTuple, Optional
 
 
 class Component(NamedTuple):
@@ -30,20 +30,17 @@ def parse_floor(line: str) -> list[Component]:
 
 def is_stable(parts: list[int]):
     as_components = [components[p] for p in parts]
-    generators = [c.kind for c in as_components if c.kind == "G"]
+    generators = [c.element for c in as_components if c.kind == "G"]
     if not generators:
         return True
 
-    return all(c for c in as_components if c.kind == "M" and c.kind in generators)
+    return all(c.element in generators for c in as_components if c.kind == "M")
 
 
 @dataclass
 class Diagram:
     floors: list[list[int]]
     elevator: int
-
-    def __lt__(self, other):
-        self.steps
 
     def try_take(self, direction: Literal["Up", "Down"], to_take: tuple[int]):
         target_floor = self.elevator - 1 if direction == "Down" else self.elevator + 1
@@ -77,42 +74,83 @@ class Diagram:
         ).__hash__()
 
 
-def parse_input(test=False) -> tuple[Diagram, list[Component]]:
+def parse_input(part) -> tuple[Diagram, list[Component]]:
     components = []
     floors = []
 
-    for line in open("test.txt" if test else "input.txt").read().splitlines():
+    input_ = "test.txt" if part == 0 else f"input{part}.txt"  
+
+    for line in open(input_).read().splitlines():
         floor = parse_floor(line)
         floors.append([i for i in range(len(components), len(components) + len(floor))])
         components += floor
 
     return (Diagram(floors=floors, elevator=0), components)
 
+def find_pair(state: Diagram, part: Component):
+    for y, floor in enumerate(state.floors):
+        for p in floor:
+            if components[p].kind != part.kind and components[p].element == part.element:
+                return y
 
-initial, components = parse_input(True)
 
+def simplify_state(state: Diagram):
+    """
+    The simplified state of a diagram is a tuple containing the elevator position followed by
+    a sorted list of tuples representing the pairs of generators and chips. Elements are not
+    encoded since two pairs of elements in two positions are interchangeable.
+    """
+    
+    if state is None:
+        return None
+    
+    pairs = []
+    elements = set()
+    for y, floor in enumerate(state.floors):
+        for c in floor:
+            part = components[c]
+            if part.element in elements:
+                continue
+            
+            other = find_pair(state, part)
+            pairs.append((tuple(sorted((y, other)))))
+    
+    pairs.sort()
+    return (state.elevator, *pairs)
+
+initial, components = parse_input(2)
+
+@dataclass(order=True)
+class PrioritizedItem:
+    priority: int
+    item: Diagram=field(compare=False)
 
 def bfs(initial: Diagram):
     queue = PriorityQueue()
-    seen = set((0, initial))
+    queue.put(PrioritizedItem(0, initial))
+    seen = set()
+    seen.add(simplify_state(initial))
 
-    while queue.not_empty:
-        steps, state = queue.get()
+    while not queue.empty():
+        item = queue.get()
+        steps = item.priority
+        state = item.item
         floor = state.floors[state.elevator]
 
         if len(state.floors[3]) == len(components):
             return steps
 
         # Try take two
-        for take in chain(combinations(floor, 2), combinations(floor, 1)):
-            for direction in ("Up", "Down"):
+        for direction in ("Up", "Down"):
+            chain_ = chain(combinations(floor, 1), combinations(floor, 2)) if direction == "Up" else chain(combinations(floor, 2), combinations(floor, 1))
+            
+            for take in chain_:
                 if (
                     next_state := state.try_take(direction, take)
-                ) and next_state not in seen:
-                    seen.add((steps + 1, next_state))
-                    queue.append((steps + 1, next_state))
+                ) and (simple_state := simplify_state(next_state)) not in seen:
+                    seen.add(simple_state)
+                    queue.put(PrioritizedItem(steps + 1, next_state))
 
     return seen
 
-
-print(bfs(initial))
+print("Part 2:", bfs(initial))
