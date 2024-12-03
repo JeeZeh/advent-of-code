83bb1a593575f15796b4b0b181386f0c3f7f2ff1
Jesse Ashmore <ashmjss@amazon.com>
2024-12-03 00:04:46 +0000

    attempt at part 2

--- 2024-py/advent/solutions/day02.py
+++ 2024-py/advent/solutions/day02.py
@@ -5,14 +5,35 @@ class Day02(Solution):
     def direction(self, a, b):
         return max(min(b - a, 1), -1)
 
-    def is_safe(self, level: list[int]):
-        initial_direction = self.direction(level[0], level[1])
-        for i in range(1, len(level)):
-            if self.direction(level[i - 1], level[i]) != initial_direction or not (
-                1 <= abs(level[i - 1] - level[i]) <= 3
-            ):
-                return False
+    def jump_is_safe(self, a, b, initial_direction):
+        if self.direction(a, b) != initial_direction or not (1 <= abs(a - b) <= 3):
+            return False
 
+        return True
+
+    def floor_is_safe(self, level: list[int], dampener=0):
+        unsafe = 0
+        initial_direction = self.direction(level[0], level[1])
+        i = 0
+        while i < len(level) - 1:
+            a, b = level[i], level[i + 1]
+            if not self.jump_is_safe(a, b, initial_direction):
+                if dampener and unsafe < dampener:
+                    try:
+                        check_right = level[i + 2]
+                        if self.jump_is_safe(a, check_right, initial_direction):
+                            unsafe += 1
+                            i += 1
+                    except:
+                        try:
+                            check_left = level[i - 1]
+                            if self.jump_is_safe(check_left, b, initial_direction):
+                                unsafe += 1
+                        except:
+                            return False
+                else:
+                    return False
+            i += 1
         return True
 
     def run(self, puzzle_input: str):
@@ -21,6 +42,7 @@ class Day02(Solution):
         ]
 
         # Part 1
-        safe_levels = sum(self.is_safe(level) for level in levels)
+        safe_levels = sum(self.floor_is_safe(level) for level in levels)
 
-        return safe_levels, None
+        safe_levels_damp = sum(self.floor_is_safe(level, 1) for level in levels)
+        return safe_levels, safe_levels_damp
