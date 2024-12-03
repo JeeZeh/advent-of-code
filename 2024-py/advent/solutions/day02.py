63a1142a34c5dc4ab057e2ffc71fe5dbec4084b9
Jesse Ashmore <ashmjss@amazon.com>
2024-12-03 00:13:53 +0000

    aoc(2024-02): 🎄

--- 2024-py/advent/solutions/day02.py
+++ 2024-py/advent/solutions/day02.py
@@ -5,36 +5,28 @@ class Day02(Solution):
     def direction(self, a, b):
         return max(min(b - a, 1), -1)
 
-    def jump_is_safe(self, a, b, initial_direction):
-        if self.direction(a, b) != initial_direction or not (1 <= abs(a - b) <= 3):
-            return False
-
-        return True
-
-    def floor_is_safe(self, level: list[int], dampener=0):
-        unsafe = 0
+    def floor_is_safe(self, level: list[int]):
         initial_direction = self.direction(level[0], level[1])
         i = 0
-        while i < len(level) - 1:
-            a, b = level[i], level[i + 1]
-            if not self.jump_is_safe(a, b, initial_direction):
-                if dampener and unsafe < dampener:
-                    try:
-                        check_right = level[i + 2]
-                        if self.jump_is_safe(a, check_right, initial_direction):
-                            unsafe += 1
-                            i += 1
-                    except:
-                        try:
-                            check_left = level[i - 1]
-                            if self.jump_is_safe(check_left, b, initial_direction):
-                                unsafe += 1
-                        except:
-                            return False
-                else:
-                    return False
-            i += 1
+        for i in range(1, len(level)):
+            a, b = level[i - 1], level[i]
+            if self.direction(a, b) != initial_direction or not (1 <= abs(a - b) <= 3):
+                return False
+
         return True
+
+    def test_floor(self, level: list[int], with_removal=False):
+        if self.floor_is_safe(level):
+            return True
+
+        if with_removal:
+            for remove_index in range(len(level)):
+                copy = [*level]
+                copy.pop(remove_index)
+                if self.floor_is_safe(copy):
+                    return True
+
+        return False
 
     def run(self, puzzle_input: str):
         levels: list[list[int]] = [
@@ -42,7 +34,12 @@ class Day02(Solution):
         ]
 
         # Part 1
-        safe_levels = sum(self.floor_is_safe(level) for level in levels)
+        safe_levels = sum(
+            self.test_floor(level, with_removal=False) for level in levels
+        )
 
-        safe_levels_damp = sum(self.floor_is_safe(level, 1) for level in levels)
+        # Part 2
+        safe_levels_damp = sum(
+            self.test_floor(level, with_removal=True) for level in levels
+        )
         return safe_levels, safe_levels_damp
