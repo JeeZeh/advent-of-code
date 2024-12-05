4035b339f6abe894722b38c508dd28633d1adb11
Jesse Ashmore <ashmjss@amazon.com>
2024-12-05 15:12:07 +0000

    aoc(2024-04): 🎄

--- 2024-py/advent/solutions/day04.py
+++ 2024-py/advent/solutions/day04.py
@@ -2,8 +2,6 @@ from dataclasses import dataclass
 from enum import Enum
 from typing import Self
 
-from aocd.models import Puzzle
-
 from advent.solution import Solution
 
 
@@ -16,15 +14,30 @@ class Pos:
         return Pos(self.x + other.x, self.y + other.y)
 
 
+class Direction(Enum):
+    UP_LEFT = Pos(-1, -1)
+    UP = Pos(0, -1)
+    UP_RIGHT = Pos(1, -1)
+    RIGHT = Pos(1, 0)
+    DOWN_RIGHT = Pos(1, 1)
+    DOWN = Pos(0, 1)
+    DOWN_LEFT = Pos(-1, 1)
+    LEFT = Pos(-1, 0)
+
+
 @dataclass
 class Grid:
-    grid: list[list[str]]
+    grid: list[list[str | None]]
     width: int
     height: int
 
     @staticmethod
     def from_string(string: str):
         grid_list = [list(line) for line in string.splitlines()]
+        return Grid.from_list(grid_list)  # type: ignore
+
+    @staticmethod
+    def from_list(grid_list: list[list[str | None]]):
         if not grid_list:
             raise ValueError("Grid empty")
 
@@ -58,16 +71,29 @@ class Grid:
             for x in range(len(self.grid[y])):
                 yield Pos(x, y)
 
+    def kernel_match(self, kernel: Self, ref: Pos):
+        for k_y in range(kernel.height):
+            for k_x in range(kernel.width):
+                local = Pos(k_x, k_y)
+                if (value := kernel.get(local)) and value != self.get(ref.add(local)):
+                    return False
+        return True
 
-class Direction(Enum):
-    UP_LEFT = Pos(-1, -1)
-    UP = Pos(0, -1)
-    UP_RIGHT = Pos(1, -1)
-    RIGHT = Pos(1, 0)
-    DOWN_RIGHT = Pos(1, 1)
-    DOWN = Pos(0, 1)
-    DOWN_LEFT = Pos(-1, 1)
-    LEFT = Pos(-1, 0)
+    def find_convolution_matches(self, kernel: Self):
+        count = 0
+        for ref_y in range(self.height - kernel.height + 1):
+            for ref_x in range(self.width - kernel.width + 1):
+                if self.kernel_match(kernel, ref=Pos(ref_x, ref_y)):
+                    count += 1
+
+        return count
+
+    def rotate(self):
+        return Grid(
+            grid=list(zip(*self.grid[::-1], strict=False)),
+            width=self.width,
+            height=self.height,
+        )
 
 
 class Day04(Solution):
@@ -89,15 +115,28 @@ class Day04(Solution):
         grid: Grid = Grid.from_string(puzzle_input)
 
         word = ["X", "M", "A", "S"]
-        count = 0
+        xmas = 0
         for pos in grid.scan():
             if grid.get(pos) == word[0]:
                 for direction in Direction:
-                    count += self.search_word(
+                    xmas += self.search_word(
                         grid, pos.add(direction.value), word[1:], direction.value
                     )
 
-        return count, None
+        kernel = Grid.from_list(
+            [
+                ["M", None, "M"],
+                [None, "A", None],
+                ["S", None, "S"],
+            ]
+        )
+
+        x_mas = 0
+        for _rotations in range(4):
+            x_mas += grid.find_convolution_matches(kernel)
+            kernel = kernel.rotate()
+
+        return xmas, x_mas
 
 
 if __name__ == "__main__":
