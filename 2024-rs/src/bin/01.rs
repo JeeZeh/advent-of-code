bd2fcaba6d17db846ae0732ad0692aaf26e206b7
Jesse Ashmore <jesse.ashmore97@gmail.com>
2024-12-08 01:07:38 +0000

    aoc(rs:2024-01): 🎄

--- 2024-rs/src/bin/01.rs
+++ 2024-rs/src/bin/01.rs
@@ -1,11 +1,31 @@
+use counter::Counter;
+
 advent_of_code::solution!(1);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    None
-}
+pub fn solve(input: &str) -> (Option<u64>, Option<u64>) {
+    let (mut left, mut right): (Vec<u64>, Vec<u64>) = input
+        .lines()
+        .map(|l| l.split_once("   ").unwrap())
+        .map(|(a, b)| {
+            (
+                u64::from_str_radix(a, 10).unwrap(),
+                u64::from_str_radix(b, 10).unwrap(),
+            )
+        })
+        .unzip();
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+    left.sort();
+    right.sort();
+
+    let right_counts = Counter::from_iter(&right);
+    (
+        Some(left.iter().zip(&right).map(|x| x.0.abs_diff(*x.1)).sum()),
+        Some(
+            left.iter()
+                .map(|num| num * right_counts.get(num).unwrap_or(&0))
+                .sum(),
+        ),
+    )
 }
 
 #[cfg(test)]
@@ -13,14 +33,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
-
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+    fn test_solve() {
+        let result = solve(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, (Some(11), Some(31)));
     }
 }
