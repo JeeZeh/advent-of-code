acd4c07e91bfff0ad8bb598f3db837b2bf2d2f8b
Jesse Ashmore <jesse.ashmore97@gmail.com>
2024-12-13 21:46:46 +0000

    aoc(rs:2024-13): 🎄

--- 2024-rs/src/bin/13.rs
+++ 2024-rs/src/bin/13.rs
@@ -16,6 +16,10 @@ impl Button {
             y: y.split_once("+").unwrap().1.parse().unwrap(),
         }
     }
+
+    fn press(&self, times: i64) -> (i64, i64) {
+        (self.x * times, self.y * times)
+    }
 }
 
 #[derive(Debug)]
@@ -48,26 +52,44 @@ impl System {
         }
     }
 
-    fn get_cost(&self) -> Option<i64> {
+    fn get_cost(&self, extend: i64) -> Option<i64> {
         let bottom_coeff = -self.b.y;
         let top_coeff = self.b.x;
         let lhs = (self.a.y * top_coeff) + (self.a.x * bottom_coeff);
-        let rhs = (self.prize.1 * top_coeff) + (self.prize.0 * bottom_coeff);
+        let rhs = ((self.prize.1 + extend) * top_coeff) + ((self.prize.0 + extend) * bottom_coeff);
         if rhs.rem_euclid(lhs) == 0 {
             let a = rhs.div_euclid(lhs);
-            let b = (self.prize.0 - self.a.x * a).div_euclid(self.b.x);
+            let b = ((self.prize.0 + extend) - self.a.x * a).div_euclid(self.b.x);
+            return self.validate_presses(a, b, extend);
+        }
+        None
+    }
+
+    fn validate_presses(&self, a: i64, b: i64, extend: i64) -> Option<i64> {
+        let press_a = self.a.press(a);
+        let press_b = self.b.press(b);
+        let p = (press_a.0 + press_b.0, press_a.1 + press_b.1);
+        if (p.0 == self.prize.0 + extend) && p.1 == self.prize.1 + extend {
             return Some((a * 3) + b);
         }
         None
     }
 }
 
+// HINT USED: Confirmed that linear equations was the right way to do this.
 pub fn solve(input: &str) -> (Option<i64>, Option<i64>) {
     let systems = input.split("\n\n").map(System::from_str).collect_vec();
 
-    let part_one: i64 = systems.iter().filter_map(|sys| sys.get_cost()).sum();
+    let part_one: i64 = systems.iter().filter_map(|sys| sys.get_cost(0)).sum();
+    let part_two: i64 = systems
+        .iter()
+        .filter_map(|sys| sys.get_cost(10000000000000))
+        .sum();
 
-    (Some((part_one as u64).try_into().unwrap()), None)
+    (
+        Some((part_one as u64).try_into().unwrap()),
+        Some((part_two as u64).try_into().unwrap()),
+    )
 }
 
 #[cfg(test)]
@@ -77,6 +99,6 @@ mod tests {
     #[test]
     fn test_solve() {
         let result = solve(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, (Some(480), None));
+        assert_eq!(result, (Some(480), Some(875318608908)));
     }
 }
