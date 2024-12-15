0ae8d625967b628042ea14dab7ca03b4606acf69
Jesse Ashmore <jesse.ashmore97@gmail.com>
2024-12-15 23:17:33 +0000

    aoc(rs:2024-15): 🎄

--- 2024-rs/src/bin/15.rs
+++ 2024-rs/src/bin/15.rs
@@ -1,4 +1,7 @@
-use std::fmt::{Display, Write};
+use std::{
+    collections::{HashSet, VecDeque},
+    fmt::{Display, Write},
+};
 
 use advent_of_code::{Direction, Grid, Pos2D};
 use itertools::Itertools;
@@ -12,7 +15,7 @@ enum Tile {
     Wall,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum WideTile {
     Space,
     BoxLeft,
@@ -83,11 +86,13 @@ fn parse_input(
             if c == '@' {
                 robot = Some((x, y));
             }
+            let tile = Tile::from(c);
             match Tile::from(c) {
                 Tile::Space => wide_row.extend(vec![WideTile::Space, WideTile::Space]),
                 Tile::Box => wide_row.extend(vec![WideTile::BoxLeft, WideTile::BoxRight]),
                 Tile::Wall => wide_row.extend(vec![WideTile::Wall, WideTile::Wall]),
             }
+            row.push(tile);
         }
         warehouse.push(row);
         wide_warehouse.push(wide_row);
@@ -113,7 +118,7 @@ fn parse_input(
     )
 }
 
-fn find_wide_box_group(
+fn find_box_destination(
     warehouse: &impl Grid<Tile>,
     from: (usize, usize),
     dir: Direction,
@@ -133,14 +138,107 @@ fn find_wide_box_group(
     None
 }
 
-fn walk_robot(moves: Vec<Direction>, mut robot: (usize, usize), warehouse: &mut Vec<Vec<Tile>>) {
+fn move_wide_group_horiz(
+    warehouse: &impl Grid<WideTile>,
+    from: (usize, usize),
+    dir: Direction,
+) -> Option<Vec<((usize, usize), WideTile)>> {
+    let mut check = from;
+    let mut to_move = Vec::new();
+    while let Some(t) = warehouse.getxy_pos(check) {
+        match t {
+            // Can't push box(es) any further.
+            WideTile::Wall => return None,
+            // Another box, keep following to the end...
+            WideTile::BoxLeft | WideTile::BoxRight => {
+                to_move.push((check, *t));
+                check = dir.step_usize(check);
+            }
+            // Box can go here!
+            WideTile::Space => break,
+        };
+    }
+
+    Some(to_move)
+}
+
+fn move_wide_group_vert(
+    warehouse: &impl Grid<WideTile>,
+    from: (usize, usize),
+    dir: Direction,
+) -> Option<Vec<((usize, usize), WideTile)>> {
+    let mut to_move = HashSet::new();
+
+    let mut queue = VecDeque::new();
+    queue.push_back(from);
+    while let Some(check) = queue.pop_front() {
+        match warehouse.getxy_pos(check) {
+            // Can't push box(es) any further.
+            Some(WideTile::Wall) => return None,
+            // Another box, keep following to the end...
+            Some(WideTile::BoxLeft) => {
+                let next_pos = dir.step_usize(check);
+                to_move.insert((check, WideTile::BoxLeft));
+                queue.push_back(next_pos);
+
+                to_move.insert((Direction::Right.step_usize(check), WideTile::BoxRight));
+                queue.push_back(Direction::Right.step_usize(next_pos));
+            }
+            Some(WideTile::BoxRight) => {
+                let next_pos = dir.step_usize(check);
+                to_move.insert((check, WideTile::BoxRight));
+                queue.push_back(next_pos);
+
+                to_move.insert((Direction::Left.step_usize(check), WideTile::BoxLeft));
+                queue.push_back(Direction::Left.step_usize(next_pos));
+            }
+            // Group might be able to move, keep checking
+            Some(WideTile::Space) => continue,
+            None => panic!("Out of bounds"),
+        }
+    }
+    Some(to_move.into_iter().collect_vec())
+}
+
+fn try_move_wide(
+    wide_warehouse: &mut Vec<Vec<WideTile>>,
+    from: (usize, usize),
+    dir: Direction,
+) -> bool {
+    let to_move = match dir {
+        Direction::Up | Direction::Down => move_wide_group_vert(wide_warehouse, from, dir),
+        Direction::Left | Direction::Right => move_wide_group_horiz(wide_warehouse, from, dir),
+    };
+
+    if let Some(group) = to_move {
+        if dir == Direction::Left || dir == Direction::Right {
+            *wide_warehouse.getxy_pos_mut(from).unwrap() = WideTile::Space;
+        }
+        if dir == Direction::Up || dir == Direction::Down {
+            // Empty everything to be moved since it's not a linear group
+            group.iter().for_each(|(old_pos, _)| {
+                *wide_warehouse.getxy_pos_mut(*old_pos).unwrap() = WideTile::Space
+            });
+        }
+        group.iter().for_each(|(old_pos, tile)| {
+            // Offset and place old tiles in new positions
+            *wide_warehouse
+                .getxy_pos_mut(dir.step_usize(*old_pos))
+                .unwrap() = *tile
+        });
+        return true;
+    }
+    false
+}
+
+fn walk_robot(moves: &[Direction], mut robot: (usize, usize), warehouse: &mut Vec<Vec<Tile>>) {
     for m in moves {
         let new_robot: (usize, usize) = m.step_usize(robot);
         match warehouse.getxy_pos(new_robot) {
             Some(Tile::Space) => robot = new_robot,
             Some(Tile::Wall) | None => continue,
             Some(Tile::Box) => {
-                if let Some(box_dest) = find_wide_box_group(&*warehouse, new_robot, m) {
+                if let Some(box_dest) = find_box_destination(&*warehouse, new_robot, *m) {
                     *warehouse.getxy_pos_mut(new_robot).unwrap() = Tile::Space;
                     *warehouse.getxy_pos_mut(box_dest).unwrap() = Tile::Box;
                     robot = new_robot;
@@ -151,7 +249,7 @@ fn walk_robot(moves: Vec<Direction>, mut robot: (usize, usize), warehouse: &mut 
 }
 
 fn walk_robot_wide(
-    moves: Vec<Direction>,
+    moves: &[Direction],
     mut robot: (usize, usize),
     wide_warehouse: &mut Vec<Vec<WideTile>>,
 ) {
@@ -161,10 +259,11 @@ fn walk_robot_wide(
             Some(WideTile::Space) => robot = new_robot,
             Some(WideTile::Wall) | None => continue,
             Some(WideTile::BoxLeft) | Some(WideTile::BoxRight) => {
-                // Find wide box group
-                // Try move group in direction (return step count)
-                // Insert spaces at original locations
-                // Insert boxes at offset locations
+                if try_move_wide(wide_warehouse, new_robot, *m) {
+                    // Only move the robot in the direction if we were able to move the
+                    // the boxes.
+                    robot = new_robot;
+                }
             }
         }
     }
@@ -173,9 +272,8 @@ fn walk_robot_wide(
 pub fn solve(input: &str) -> (Option<u64>, Option<u64>) {
     let (mut warehouse, mut wide_warehouse, mut robot, moves) = parse_input(input);
 
-    walk_robot(moves, robot, &mut warehouse);
+    walk_robot(&moves, robot, &mut warehouse);
     warehouse.show_display();
-    wide_warehouse.show_display();
 
     let part_one: usize = warehouse
         .scan()
@@ -188,7 +286,20 @@ pub fn solve(input: &str) -> (Option<u64>, Option<u64>) {
         })
         .sum();
 
-    (Some(part_one as u64), None)
+    walk_robot_wide(&moves, (robot.0 * 2, robot.1), &mut wide_warehouse);
+    wide_warehouse.show_display();
+    let part_two: usize = wide_warehouse
+        .scan()
+        .filter_map(|(pos, tile)| {
+            if matches!(tile, WideTile::BoxLeft) {
+                Some((pos.1 * 100) + pos.0)
+            } else {
+                None
+            }
+        })
+        .sum();
+
+    (Some(part_one as u64), Some(part_two as u64))
 }
 
 #[cfg(test)]
@@ -198,6 +309,6 @@ mod tests {
     #[test]
     fn test_solve() {
         let result = solve(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, (Some(10092), None));
+        assert_eq!(result, (Some(10092), Some(9021)));
     }
 }
