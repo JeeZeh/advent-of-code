bc6c2c4656965a6c46668ae870c39452fe9b9604
Jesse Ashmore <jesse.ashmore97@gmail.com>
2024-12-17 19:03:03 +0000

    solve(rs:2024-17): part 1

--- 2024-rs/src/bin/17.rs
+++ 2024-rs/src/bin/17.rs
@@ -12,7 +12,7 @@ const REG_C: usize = 2;
 struct Cpu {
     registers: [i64; 3],
     inst_ptr: usize,
-    output: VecDeque<i64>,
+    output: Vec<i64>,
 }
 
 enum Op {
@@ -28,7 +28,7 @@ impl Op {
                 if *v >= 0 && *v <= 3 {
                     return *v;
                 } else if *v < 7 {
-                    return registers[(v - 3) as usize];
+                    return registers[(v - 4) as usize];
                 } else {
                     panic!("Unexpected operand: {}", v);
                 }
@@ -58,7 +58,7 @@ impl Inst {
         Some(match opcode {
             0 => Inst::Adv(Op::Combo(prog[ptr + 1])),
             1 => Inst::Bxl(Op::Literal(prog[ptr + 1])),
-            2 => Inst::Bst(Op::Literal(prog[ptr + 1])),
+            2 => Inst::Bst(Op::Combo(prog[ptr + 1])),
             3 => Inst::Jnz(Op::Literal(prog[ptr + 1])),
             4 => Inst::Bxc,
             5 => Inst::Out(Op::Combo(prog[ptr + 1])),
@@ -67,14 +67,46 @@ impl Inst {
             _ => panic!("Unrecognized opcode: {}", opcode),
         })
     }
+
+    fn execute(&self, registers: &mut [i64; 3]) -> Option<i64> {
+        match self {
+            Inst::Adv(op) => {
+                registers[REG_A] =
+                    registers[REG_A].div_floor(2_i64.pow(op.resolve(&registers) as u32));
+            }
+            Inst::Bxl(op) => {
+                registers[REG_B] = registers[REG_B].bitxor(op.resolve(&registers));
+            }
+            Inst::Bst(op) => {
+                registers[REG_B] = op.resolve(&registers).rem_euclid(8);
+            }
+            Inst::Bxc => {
+                registers[REG_B] = registers[REG_B].bitxor(registers[REG_C]);
+            }
+            Inst::Out(op) => {
+                return Some(op.resolve(&registers).rem_euclid(8));
+            }
+            Inst::Bdv(op) => {
+                registers[REG_B] =
+                    registers[REG_A].div_floor(2_i64.pow(op.resolve(&registers) as u32));
+            }
+            Inst::Cdv(op) => {
+                registers[REG_C] =
+                    registers[REG_A].div_floor(2_i64.pow(op.resolve(&registers) as u32));
+            }
+            // Don't do anything with jumps just yet.
+            Inst::Jnz(_) => (),
+        };
+        None
+    }
 }
 
 impl Cpu {
     fn init(reg_a: i64, reg_b: i64, reg_c: i64) -> Self {
         Cpu {
-            registers: [0, 0, 0],
+            registers: [reg_a, reg_b, reg_c],
             inst_ptr: 0,
-            output: VecDeque::new(),
+            output: Vec::new(),
         }
     }
 
@@ -82,52 +114,31 @@ impl Cpu {
         self.output.drain(0..self.output.len()).join(",")
     }
 
+    fn run_till_completion(&mut self, program: &[i64]) {
+        while self.tick(program).is_some() {
+            continue;
+        }
+    }
+
     fn tick(&mut self, program: &[i64]) -> Option<usize> {
         if let Some(inst) = Inst::read(&program, self.inst_ptr) {
-            let new_pointer: Option<usize> = match inst {
-                Inst::Adv(op) => {
-                    self.registers[REG_A] =
-                        self.registers[REG_A].div_floor(op.resolve(&self.registers).pow(2));
-                    None
-                }
-                Inst::Bxl(op) => {
-                    self.registers[REG_B] =
-                        self.registers[REG_B].bitxor(op.resolve(&self.registers));
-                    None
-                }
-                Inst::Bst(op) => {
-                    self.registers[REG_B] = op.resolve(&self.registers).rem_euclid(8);
-                    None
-                }
-                Inst::Jnz(op) => {
-                    if self.registers[REG_A] != 0 {
-                        return Some(op.resolve(&self.registers) as usize);
-                    }
-                    None
-                }
-                Inst::Bxc => {
-                    self.registers[REG_B] = self.registers[REG_B].bitxor(self.registers[REG_C]);
-                    None
-                }
-                Inst::Out(op) => {
-                    self.output
-                        .push_front(op.resolve(&self.registers).rem_euclid(8));
-                    None
-                }
-                Inst::Bdv(op) => {
-                    self.registers[REG_B] =
-                        self.registers[REG_A].div_floor(op.resolve(&self.registers).pow(2));
-                    None
-                }
-                Inst::Cdv(op) => {
-                    self.registers[REG_C] =
-                        self.registers[REG_A].div_floor(op.resolve(&self.registers).pow(2));
-                    None
-                }
-            };
+            // Execute instruction, store output if any.
+            if let Some(output) = inst.execute(&mut self.registers) {
+                self.output.push(output);
+            }
 
-            // Set new pointer location or increment by 2.
-            self.inst_ptr = new_pointer.unwrap_or(self.inst_ptr + 2);
+            // Advance pointer.
+            match inst {
+                Inst::Jnz(op) => {
+                    if self.registers[REG_A] == 0 {
+                        self.inst_ptr += 2;
+                    } else {
+                        self.inst_ptr = op.resolve(&self.registers) as usize;
+                    }
+                }
+                _ => self.inst_ptr += 2,
+            }
+
             return Some(self.inst_ptr);
         }
         None
@@ -150,9 +161,7 @@ pub fn solve(input: &str) -> (Option<String>, Option<String>) {
         .map(|i| i.parse::<i64>().unwrap())
         .collect_vec();
 
-    while let Some(ptr) = cpu.tick(&program) {
-        continue;
-    }
+    cpu.run_till_completion(&program);
 
     (Some(cpu.drain_output()), None)
 }
@@ -165,5 +174,47 @@ mod tests {
     fn test_solve() {
         let result = solve(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, (Some(String::from("4,6,3,5,6,3,5,2,1,0")), None));
+    }
+
+    /// If register C contains 9, the program 2,6 would set register B to 1.
+    #[test]
+    fn test_one() {
+        let mut cpu = Cpu::init(0, 0, 9);
+
+        cpu.run_till_completion(&vec![2, 6]);
+        assert_eq!(cpu.registers[REG_B], 1);
+    }
+
+    /// If register A contains 10, the program 5,0,5,1,5,4 would output 0,1,2.
+    #[test]
+    fn test_two() {
+        let mut cpu = Cpu::init(10, 0, 0);
+        cpu.run_till_completion(&vec![5, 0, 5, 1, 5, 4]);
+        assert_eq!(cpu.drain_output(), "0,1,2");
+    }
+
+    /// If register A contains 2024, the program 0,1,5,4,3,0 would output 4,2,5,6,7,7,7,7,3,1,0 and leave 0 in register A.
+    #[test]
+    fn test_three() {
+        let mut cpu = Cpu::init(2024, 0, 0);
+        cpu.run_till_completion(&vec![0, 1, 5, 4, 3, 0]);
+        assert_eq!(cpu.drain_output(), "4,2,5,6,7,7,7,7,3,1,0");
+        assert_eq!(cpu.registers[REG_A], 0);
+    }
+
+    /// If register B contains 29, the program 1,7 would set register B to 26.
+    #[test]
+    fn test_four() {
+        let mut cpu = Cpu::init(0, 29, 9);
+        cpu.run_till_completion(&vec![1, 7]);
+        assert_eq!(cpu.registers[REG_B], 26);
+    }
+
+    /// If register B contains 2024 and register C contains 43690, the program 4,0 would set register B to 44354.
+    #[test]
+    fn test_five() {
+        let mut cpu = Cpu::init(0, 2024, 43690);
+        cpu.run_till_completion(&vec![4, 0]);
+        assert_eq!(cpu.registers[REG_B], 44354);
     }
 }
