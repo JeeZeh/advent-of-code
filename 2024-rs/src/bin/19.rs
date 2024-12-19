d41f46b5d465bfee1e58c5825feb74f179f6e2d7
Jesse Ashmore <jesse.ashmore97@gmail.com>
2024-12-19 17:19:09 +0000

    solve(rs:2024-19): 🎄

--- 2024-rs/src/bin/19.rs
+++ 2024-rs/src/bin/19.rs
@@ -1,10 +1,14 @@
-use std::{collections::VecDeque, ops::Range};
+use std::{
+    collections::{HashMap, VecDeque},
+    mem,
+    ops::Range,
+};
 
 use itertools::Itertools;
 
 advent_of_code::solution!(19);
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Color {
     White,
     Blue,
@@ -30,74 +34,43 @@ trait Towel {}
 
 impl Towel for Vec<Color> {}
 
-fn find_configuration(towels: &[Vec<Color>], to_make: &[Color]) -> bool {
-    let mut queue = VecDeque::new();
-    queue.push_back(to_make);
-
-    while let Some(remaining) = queue.pop_back() {
-        if remaining.is_empty() {
-            return true;
-        }
-
-        for towel in towels {
-            if remaining.starts_with(towel) {
-                queue.push_back(&remaining[towel.len()..]);
-            }
-        }
+fn find_configurations<'a>(
+    towels: &[Vec<Color>],
+    to_make: &'a [Color],
+    memo: &mut HashMap<&'a [Color], usize>,
+) -> usize {
+    if to_make.is_empty() {
+        return 1;
     }
 
-    false
-}
-
-fn merge_overlapping_intervals(arr: &mut Vec<Range<usize>>) -> Vec<Range<usize>> {
-    arr.sort_by(|a, b| a.start.cmp(&b.start));
-
-    let mut result: Vec<Range<usize>> = Vec::new();
-    result.push(arr[0].clone());
-
-    for i in 1..arr.len() {
-        let current: Range<usize> = arr[i].clone();
-        let j: usize = result.len() - 1;
-
-        if current.start >= result[j].start && current.start <= result[j].end {
-            result[j].end = current.end.max(result[j].end);
-        } else {
-            result.push(current);
-        }
-    }
-    result
-}
-
-fn find_configuration_v2(towels: &[Vec<Color>], to_make: &[Color]) -> bool {
-    let mut ranges = Vec::new();
-
-    for i in 0..to_make.len() {
-        for towel in towels {
-            if to_make[i..].starts_with(&towel) {
-                ranges.push(i..i + towel.len());
-            }
-        }
+    if let Some(&cached) = memo.get(to_make) {
+        return cached;
     }
 
-    let merged = merge_overlapping_intervals(&mut ranges);
-    return merged.len() == 1 && merged[0].start == 0 && merged[0].end == to_make.len();
+    let mut configurations = 0;
+    for towel in towels {
+        if to_make.starts_with(towel) {
+            configurations += find_configurations(towels, &to_make[towel.len()..], memo);
+        }
+    }
+    memo.insert(to_make, configurations);
+    configurations
 }
 
-pub fn solve(input: &str) -> (Option<u64>, Option<u64>) {
+// HINT USED: Confirmed that memoization would work.
+pub fn solve(input: &str) -> (Option<usize>, Option<usize>) {
     let (towels, patterns) = parse_input(input);
 
-    let mut valid = 0;
-    for pattern in &patterns {
-        println!("Testing {pattern:?}");
-        if find_configuration_v2(&towels, &pattern) {
-            println!("Valid");
-            if cfg!(debug_assertions) {}
-            valid += 1;
-        };
-    }
+    let mut memo = HashMap::new();
+    let possible_configurations = patterns
+        .iter()
+        .map(|p| find_configurations(&towels, p, &mut memo))
+        .collect_vec();
 
-    println!("{valid}/{}", &patterns.len());
-    (Some(valid), None)
+    (
+        Some(possible_configurations.iter().filter(|&&c| c > 0).count()),
+        Some(possible_configurations.iter().sum()),
+    )
 }
 
 fn parse_input(input: &str) -> (Vec<Vec<Color>>, Vec<Vec<Color>>) {
@@ -118,6 +91,7 @@ fn parse_input(input: &str) -> (Vec<Vec<Color>>, Vec<Vec<Color>>) {
         .filter(|l| !l.is_empty())
         .map(|l| l.chars().map(Color::from_char).collect())
         .collect();
+    
     if cfg!(debug_assertions) {
         println!("Patterns: {patterns:?}");
     }
@@ -130,24 +104,8 @@ mod tests {
     use super::*;
     #[test]
     fn test_solve() {
-        let result = solve(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, (Some(6), None));
-    }
-
-    #[test]
-    fn test_invalid() {
-        let towels = vec![
-            vec![Color::Black, Color::White, Color::Blue],
-            vec![Color::White, Color::Red],
-            vec![Color::Red, Color::Black],
-            vec![Color::Green, Color::Black],
-            vec![Color::Black, Color::Red],
-            vec![Color::Red],
-            vec![Color::Black],
-            vec![Color::Green],
-        ];
-
-        let pattern = vec![Color::Blue, Color::Black, Color::White, Color::Blue];
-        assert_eq!(find_configuration_v2(&towels, &pattern), false);
+        let result: (Option<usize>, Option<usize>) =
+            solve(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, (Some(6), Some(16)));
     }
 }
