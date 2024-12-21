033c3dd066e7f272bc8ae4b0b0c205d6b91bce3f
Jesse Ashmore <jesse.ashmore97@gmail.com>
2024-12-21 20:41:52 +0000

    chore(rs:2024-21): implemented someone else's solution

--- 2024-rs/src/bin/21.rs
+++ 2024-rs/src/bin/21.rs
@@ -1,20 +1,34 @@
-use std::fmt::{Display, Write};
+use std::{
+    cmp::Reverse,
+    collections::BinaryHeap,
+    fmt::{Display, Write},
+};
 
-use advent_of_code::{Direction, Grid, Pos2D};
+use advent_of_code::{Direction, Grid};
 use itertools::Itertools;
 
 advent_of_code::solution!(21);
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Ord, PartialOrd)]
 enum Key {
     Up,
+    Right,
     Down,
     Left,
-    Right,
     Activate,
 }
 
 impl Key {
+    fn from_movement(from: (usize, usize), to: (usize, usize)) -> Self {
+        match (to.0.cmp(&from.0), to.1.cmp(&from.1)) {
+            (std::cmp::Ordering::Less, _) => Key::Left,
+            (std::cmp::Ordering::Greater, _) => Key::Right,
+            (_, std::cmp::Ordering::Less) => Key::Up,
+            (_, std::cmp::Ordering::Greater) => Key::Down,
+            (std::cmp::Ordering::Equal, std::cmp::Ordering::Equal) => Key::Activate,
+        }
+    }
+
     fn from(c: char) -> Key {
         match c {
             '^' => Key::Up,
@@ -53,9 +67,6 @@ impl Display for Key {
     }
 }
 
-const ACTIVATE: u8 = u8::MAX;
-const EMPTY: u8 = u8::MAX - 1;
-
 #[rustfmt::skip]
 /// +---+---+---+
 /// | 7 | 8 | 9 |
@@ -73,67 +84,6 @@ const NUMERIC_KEYPAD: [[char; 3]; 4] = [
     [' ', '0', 'A'],
 ];
 
-fn get_numeric_inputs(keycode: &[char]) -> Vec<Key> {
-    // If on 0, first move can't be left
-    // If on 1, first move can't be down
-
-    // Start on A
-    let mut current = (2, 3);
-    let mut sequence = Vec::new();
-
-    for key in keycode {
-        let key_dest = NUMERIC_KEYPAD
-            .scan()
-            .find(|(_, k)| *k == key)
-            .expect("Couldn't find numeric key position")
-            .0;
-
-        while let Some(diff) = diff(key_dest, current) {
-            let mut press = None;
-
-            press = match diff.0.signum() {
-                -1 => {
-                    if current != (1, 3) {
-                        Some(Key::Left)
-                    } else {
-                        None
-                    }
-                }
-                1 => Some(Key::Right),
-                _ => None,
-            };
-
-            if press.is_none() {
-                press = match diff.1.signum() {
-                    -1 => Some(Key::Up),
-                    1 => {
-                        if current != (0, 2) {
-                            Some(Key::Down)
-                        } else {
-                            None
-                        }
-                    }
-                    _ => None,
-                };
-            }
-
-            // println!("{current:?}, {diff:?}, {press:?}");
-            current = press.as_ref().unwrap().step(current);
-            sequence.push(press.unwrap());
-        }
-        sequence.push(Key::Activate);
-    }
-
-    sequence
-}
-
-fn diff(a: (usize, usize), b: (usize, usize)) -> Option<(i32, i32)> {
-    if a == b {
-        return None;
-    }
-    Some((a.0 as i32 - b.0 as i32, a.1 as i32 - b.1 as i32))
-}
-
 #[rustfmt::skip]
 ///     +---+---+
 ///     | ^ | A |
@@ -145,60 +95,128 @@ const DIRECTIONAL_KEYPAD: [[char; 3]; 2] = [
     ['<', 'v', '>'],
 ];
 
-fn get_directional_inputs(keycode_inputs: &[Key]) -> Vec<Key> {
-    // If on ^, first move can't be left
-    // If on <, first move can't be up
+/// The cost of pressing a button on the directional pad with `level` levels of indirection
+fn build_press_costs(grid: &impl Grid<char>, level: i32) -> [[usize; 5]; 5] {
+    use Key::*;
+    if level == 0 {
+        // uniform of 1 cost for user
+        [[1; 5]; 5]
+    } else {
+        let mut current_press_costs = [[0; 5]; 5];
+        let previous_press_costs = build_press_costs(grid, level - 1);
 
-    // Start on A
-    let mut current = (2, 0);
-    let mut sequence = Vec::new();
+        for from in [Up, Right, Down, Left, Activate] {
+            // seen does not guarantee that each button will be evaluated just once; just that
+            // we'll stop evaluating them quickly
+            let mut seen = [false; 5];
+            let from_pos = grid
+                .find_item(from.as_char())
+                .expect("Couldn't find numeric key position");
 
-    for key in keycode_inputs.iter().map(Key::as_char) {
-        let key_dest = DIRECTIONAL_KEYPAD
-            .scan()
-            .find(|(_, &k)| k == key)
-            .expect("Couldn't find directional key position")
-            .0;
-
-        while let Some(diff) = diff(key_dest, current) {
-            let mut press = None;
-
-            press = match diff.0.signum() {
-                -1 => {
-                    if current != (1, 0) {
-                        Some(Key::Left)
-                    } else {
-                        None
+            let mut queue = BinaryHeap::new();
+            queue.push((Reverse(0), from_pos, Activate));
+            while let Some((Reverse(cost), pos, button)) = queue.pop() {
+                let cd = Key::from(*grid.getxy_pos(pos).unwrap());
+                if cost > 0 && button == Activate {
+                    // first cost will be optimal, so if we have one don't update
+                    if current_press_costs[from as usize][cd as usize] == 0 {
+                        current_press_costs[from as usize][cd as usize] = cost;
                     }
+                } else {
+                    queue.push((
+                        Reverse(cost + previous_press_costs[button as usize][Activate as usize]),
+                        pos,
+                        Activate,
+                    ));
                 }
-                1 => Some(Key::Right),
-                _ => None,
-            };
+                seen[cd as usize] = true;
 
-            if press.is_none() {
-                press = match diff.1.signum() {
-                    -1 => {
-                        if current != (0, 1) {
-                            Some(Key::Up)
-                        } else {
-                            None
-                        }
+                for (n, &check) in grid.neighbours_axis(pos) {
+                    if check == ' ' {
+                        continue;
                     }
-                    1 => Some(Key::Down),
-                    _ => None,
-                };
+
+                    let d = Key::from(check);
+                    if seen[d as usize] {
+                        continue;
+                    }
+                    let next_button = Key::from_movement(pos, n);
+                    queue.push((
+                        Reverse(cost + previous_press_costs[button as usize][next_button as usize]),
+                        n,
+                        next_button,
+                    ));
+                }
             }
-
-            // println!("{current:?}, {diff:?}, {press:?}");
-            current = press.as_ref().unwrap().step(current);
-            sequence.push(press.unwrap());
         }
-        sequence.push(Key::Activate);
+        current_press_costs
     }
-
-    sequence
 }
 
+/// Computes the shortest path between two numerical buttons taking into account
+/// the cost of pressing the directional buttons from the previously-calculated
+/// costs (based on levels of indirection).
+fn shortest_path(
+    grid: &impl Grid<char>,
+    press_costs: [[usize; 5]; 5],
+    from_n: char,
+    to_n: char,
+) -> usize {
+    use Key::*;
+    let from = grid.find_item(from_n).unwrap();
+    let to = grid.find_item(to_n).unwrap();
+
+    let mut queue = BinaryHeap::new();
+    queue.push((Reverse(0), from, Activate));
+
+    while let Some((Reverse(cost), pos, button)) = queue.pop() {
+        if pos == to {
+            if button == Activate {
+                return cost;
+            } else {
+                queue.push((
+                    Reverse(cost + press_costs[button as usize][Activate as usize]),
+                    pos,
+                    Activate,
+                ));
+            }
+        } else {
+            for (n, &check) in grid.neighbours_axis(pos) {
+                if check == ' ' {
+                    continue;
+                }
+                let nd = Key::from_movement(pos, n);
+                queue.push((
+                    Reverse(cost + press_costs[button as usize][nd as usize]),
+                    n,
+                    nd,
+                ));
+            }
+        }
+    }
+
+    unreachable!()
+}
+
+fn total_input_costs(grid: &impl Grid<char>, press_costs: [[usize; 5]; 5], line: &[char]) -> usize {
+    // Starting on A
+    let moves = [&['A'], line].concat();
+    let steps = moves
+        .windows(2)
+        .map(|w| shortest_path(grid, press_costs, w[0], w[1]))
+        .sum::<usize>();
+
+    steps
+        * line
+            .iter()
+            .filter(|c| c.is_numeric())
+            .join("")
+            .parse::<usize>()
+            .unwrap()
+}
+
+// HINT (SOLUTION) USED: https://www.reddit.com/r/adventofcode/comments/1hj2odw/2024_day_21_solutions/m34qus1/
+// Tried a faulty approach for part one, didn't have the intuition or motivation to figure it out from scratch.
 pub fn solve(input: &str) -> (Option<usize>, Option<usize>) {
     let keycodes = input
         .lines()
@@ -206,25 +224,19 @@ pub fn solve(input: &str) -> (Option<usize>, Option<usize>) {
         .map(|l| l.chars().collect_vec())
         .collect_vec();
 
-    let mut complexity = 0;
-    for keycode in keycodes {
-        let numeric = get_numeric_inputs(&keycode);
-        println!("{numeric:?}");
-        let directional = get_directional_inputs(&numeric);
-        println!("{directional:?}");
-        let remote = get_directional_inputs(&directional);
-        println!("{}", remote.iter().map(Key::as_char).join(""));
-        println!("{}", remote.len());
-        complexity += remote.len()
-            * keycode
-                .iter()
-                .filter(|c| c.is_numeric())
-                .join("")
-                .parse::<usize>()
-                .unwrap();
-    }
+    let input_costs = build_press_costs(&DIRECTIONAL_KEYPAD, 2);
+    let part_one: usize = keycodes
+        .iter()
+        .map(|keycode| total_input_costs(&NUMERIC_KEYPAD, input_costs, keycode))
+        .sum();
 
-    (Some(complexity), None)
+    let input_costs = build_press_costs(&DIRECTIONAL_KEYPAD, 25);
+    let part_two: usize = keycodes
+        .iter()
+        .map(|keycode| total_input_costs(&NUMERIC_KEYPAD, input_costs, keycode))
+        .sum();
+
+    (Some(part_one), Some(part_two))
 }
 
 #[cfg(test)]
@@ -235,15 +247,5 @@ mod tests {
     fn test_solve() {
         let result = solve(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, (Some(126384), None));
-    }
-
-    #[test]
-    fn test_keypad() {
-        let possible = vec![
-            "<A^A>^^AvvvA".chars().map(Key::from).collect_vec(),
-            "<A^A^>^AvvvA".chars().map(Key::from).collect_vec(),
-            "<A^A^^>AvvvA".chars().map(Key::from).collect_vec(),
-        ];
-        assert!(possible.contains(&get_numeric_inputs(&"029A".chars().collect_vec())));
     }
 }
