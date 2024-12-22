df57c093f9f9b007d12b0a3a5ab44cdabd5239e7
Jesse Ashmore <jesse.ashmore97@gmail.com>
2024-12-22 23:54:35 +0000

    optimize(rs:2024-22): unsuccessful

--- 2024-rs/src/bin/22.rs
+++ 2024-rs/src/bin/22.rs
@@ -8,14 +8,14 @@ use itertools::Itertools;
 advent_of_code::solution!(22);
 
 struct Buyer {
-    secret: u64,
+    secret: u32,
 }
 
 impl FromStr for Buyer {
     type Err = ParseIntError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.parse::<u64>() {
+        match s.parse::<u32>() {
             Ok(secret) => Ok(Buyer { secret }),
             Err(e) => Err(e),
         }
@@ -32,7 +32,7 @@ impl Buyer {
     }
 }
 
-pub fn solve(input: &str) -> (Option<u64>, Option<u64>) {
+pub fn solve(input: &str) -> (Option<u32>, Option<u32>) {
     let mut buyers = lines_no_empty(input)
         .map(|l| Buyer::from_str(l).unwrap())
         .collect_vec();
@@ -42,23 +42,22 @@ pub fn solve(input: &str) -> (Option<u64>, Option<u64>) {
     for buyer in buyers.iter_mut() {
         (0..2000)
             .map(|_| {
-                let old_one = (buyer.secret % 10) as i8;
-                let new_one = (buyer.evolve().secret % 10) as i8;
+                let old_one = buyer.secret % 10;
+                let new_one = buyer.evolve().secret % 10;
                 ((old_one - new_one), new_one)
             })
             .map_windows(|[(a, _), (b, _), (c, _), (d, val)]| (hash(*a, *b, *c, *d), *val))
             .unique_by(|f| f.0)
-            .for_each(|(seq, val)| hashed[seq as usize] += val as u64);
+            .for_each(|(seq, val)| hashed[seq as usize] += val as u32);
     }
-    let sum_2000: u64 = buyers.iter().map(|b| b.secret).sum();
+    let sum_2000 = buyers.iter().map(|b| b.secret as u32).sum::<u32>();
     let &best_sequence = hashed.iter().max().unwrap();
 
     (Some(sum_2000), Some(best_sequence))
 }
 
-fn hash(a: i8, b: i8, c: i8, d: i8) -> u32 {
-    (((a as u32 + 10) << 15) + ((b as u32 + 10) << 10) + ((c as u32 + 10) << 5) + (d as u32 + 10))
-        & 0xFFFFF
+fn hash(a: u32, b: u32, c: u32, d: u32) -> u32 {
+    (((a + 10) << 15) + ((b + 10) << 10) + ((c + 10) << 5) + (d + 10)) & 0xFFFFF
 }
 
 #[cfg(test)]
