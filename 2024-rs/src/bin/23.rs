d9fd6cb536a7a45002789017e0d9e7fcb5360d8d
Jesse Ashmore <jesse.ashmore97@gmail.com>
2024-12-23 23:06:27 +0000

    solve(rs:2024-23): 🎄

--- 2024-rs/src/bin/23.rs
+++ 2024-rs/src/bin/23.rs
@@ -1,4 +1,5 @@
 use core::net;
+use std::borrow::Borrow;
 use std::iter::once;
 
 use advent_of_code::{lines_no_empty, Pairs};
@@ -11,133 +12,35 @@ use std::collections::{HashMap, HashSet};
 use std::fmt::Debug;
 use std::hash::Hash;
 
-#[derive(Debug)]
-/// A Union-Find (Disjoint Set) data structure implementation in Rust.
-///
-/// The Union-Find data structure keeps track of elements partitioned into
-/// disjoint (non-overlapping) sets.
-/// It provides near-constant-time operations to add new sets, to find the
-/// representative of a set, and to merge sets.
-///
-/// See: https://github.com/TheAlgorithms/Rust/blob/master/src/data_structures/union_find.rs
-pub struct UnionFind<T: Debug + Eq + Hash> {
-    payloads: HashMap<T, usize>, // Maps values to their indices in the parent_links array.
-    parent_links: Vec<usize>,    // Holds the parent pointers; root elements are their own parents.
-    sizes: Vec<usize>,           // Holds the sizes of the sets.
-    count: usize,                // Number of disjoint sets.
-}
-
-impl<T: Debug + Eq + Hash> UnionFind<T> {
-    /// Creates an empty Union-Find structure with a specified capacity.
-    pub fn with_capacity(capacity: usize) -> Self {
-        Self {
-            parent_links: Vec::with_capacity(capacity),
-            sizes: Vec::with_capacity(capacity),
-            payloads: HashMap::with_capacity(capacity),
-            count: 0,
+fn bron_kerbosch<'a>(
+    r: HashSet<&'a str>,
+    mut p: HashSet<&'a str>,
+    mut x: HashSet<&'a str>,
+    neighbors: &HashMap<&'a str, HashSet<&'a str>>,
+    clique: &mut HashSet<&'a str>,
+) {
+    if p.is_empty() && x.is_empty() {
+        if r.len() > clique.len() {
+            *clique = r;
         }
+        return;
     }
 
-    /// Inserts a new item (disjoint set) into the data structure.
-    pub fn insert(&mut self, item: T) {
-        let key = self.payloads.len();
-        self.parent_links.push(key);
-        self.sizes.push(1);
-        self.payloads.insert(item, key);
-        self.count += 1;
-    }
-
-    /// Returns the root index of the set containing the given value, or `None` if it doesn't exist.
-    pub fn find(&mut self, value: &T) -> Option<usize> {
-        self.payloads
-            .get(value)
-            .copied()
-            .map(|key| self.find_by_key(key))
-    }
-
-    /// Unites the sets containing the two given values. Returns:
-    /// - `None` if either value hasn't been inserted,
-    /// - `Some(true)` if two disjoint sets have been merged,
-    /// - `Some(false)` if both elements were already in the same set.
-    pub fn union(&mut self, first_item: &T, sec_item: &T) -> Option<bool> {
-        let (first_root, sec_root) = (self.find(first_item), self.find(sec_item));
-        match (first_root, sec_root) {
-            (Some(first_root), Some(sec_root)) => Some(self.union_by_key(first_root, sec_root)),
-            _ => None,
-        }
-    }
-
-    /// Finds the root of the set containing the element with the given index.
-    fn find_by_key(&mut self, key: usize) -> usize {
-        if self.parent_links[key] != key {
-            self.parent_links[key] = self.find_by_key(self.parent_links[key]);
-        }
-        self.parent_links[key]
-    }
-
-    /// Unites the sets containing the two elements identified by their indices.
-    fn union_by_key(&mut self, first_key: usize, sec_key: usize) -> bool {
-        let (first_root, sec_root) = (self.find_by_key(first_key), self.find_by_key(sec_key));
-
-        if first_root == sec_root {
-            return false;
-        }
-
-        match self.sizes[first_root].cmp(&self.sizes[sec_root]) {
-            Ordering::Less => {
-                self.parent_links[first_root] = sec_root;
-                self.sizes[sec_root] += self.sizes[first_root];
-            }
-            _ => {
-                self.parent_links[sec_root] = first_root;
-                self.sizes[first_root] += self.sizes[sec_root];
-            }
-        }
-
-        self.count -= 1;
-        true
-    }
-
-    /// Checks if two items belong to the same set.
-    pub fn is_same_set(&mut self, first_item: &T, sec_item: &T) -> bool {
-        matches!((self.find(first_item), self.find(sec_item)), (Some(first_root), Some(sec_root)) if first_root == sec_root)
-    }
-
-    /// Returns the number of disjoint sets.
-    pub fn count(&self) -> usize {
-        self.count
+    for &vertex in p.clone().iter() {
+        let connected = neighbors.get(vertex).unwrap();
+        bron_kerbosch(
+            r.iter().copied().chain(once(vertex)).collect(),
+            p.intersection(connected).copied().collect(),
+            x.intersection(connected).copied().collect(),
+            neighbors,
+            clique,
+        );
+        p.remove(vertex);
+        x.insert(vertex);
     }
 }
 
-impl<T: Debug + Eq + Hash> Default for UnionFind<T> {
-    fn default() -> Self {
-        Self {
-            parent_links: Vec::default(),
-            sizes: Vec::default(),
-            payloads: HashMap::default(),
-            count: 0,
-        }
-    }
-}
-
-impl<T: Debug + Eq + Hash> FromIterator<T> for UnionFind<T> {
-    /// Creates a new UnionFind data structure from an iterable of disjoint elements.
-    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
-        let mut uf = UnionFind::default();
-        for item in iter {
-            uf.insert(item);
-        }
-        uf
-    }
-}
-
-// struct Node<'a> {
-//     val: &'a str,
-//     incoming: Vec<&'a str>,
-//     outgoing: Vec<>
-// }
-
-pub fn solve(input: &str) -> (Option<u64>, Option<u64>) {
+pub fn solve(input: &str) -> (Option<String>, Option<String>) {
     let connections = lines_no_empty(input)
         .map(|l| l.split_once("-").unwrap())
         .collect_vec();
@@ -147,17 +50,17 @@ pub fn solve(input: &str) -> (Option<u64>, Option<u64>) {
         .unique()
         .collect_vec();
 
-    let mut network: HashMap<&str, Vec<&str>> = HashMap::new();
+    let mut network: HashMap<&str, HashSet<&str>> = HashMap::new();
     connections.iter().for_each(|(left, right)| {
-        network.entry(left).or_default().push(right);
-        network.entry(right).or_default().push(left);
+        network.entry(left).or_default().insert(right);
+        network.entry(right).or_default().insert(left);
     });
 
     let mut combinations_with_t: HashSet<[&str; 3]> = HashSet::new();
     for &a in &computers {
-        for (b, c) in network.get(a).unwrap().pairs() {
+        for (b, c) in network.get(a).unwrap().iter().collect_vec().pairs() {
             // HINT: Check if pairs are connected.
-            if network.get(b).unwrap().contains(&c) && network.get(c).unwrap().contains(&b) {
+            if network.get(b).unwrap().contains(c) && network.get(c).unwrap().contains(b) {
                 if a.starts_with("t") || c.starts_with("t") || c.starts_with("t") {
                     let mut group = [a, b, c];
                     group.sort();
@@ -167,6 +70,17 @@ pub fn solve(input: &str) -> (Option<u64>, Option<u64>) {
         }
     }
 
+    // HINT: Found Bron-Kerbosch, looked for existing Rust implementations.
+    let mut largest_clique_sink: HashSet<&str> = HashSet::new();
+    bron_kerbosch(
+        Default::default(),
+        network.keys().cloned().collect(),
+        Default::default(),
+        &network,
+        &mut largest_clique_sink,
+    );
+    let password = largest_clique_sink.iter().sorted().join(",");
+
     if cfg!(debug_assertions) {
         println!("{connections:?}");
         println!("{computers:?}");
@@ -174,7 +88,10 @@ pub fn solve(input: &str) -> (Option<u64>, Option<u64>) {
         println!("{:?}", combinations_with_t.len());
     }
 
-    (Some(combinations_with_t.len() as u64), None)
+    (
+        Some(format!("{}", combinations_with_t.len())),
+        Some(password),
+    )
 }
 
 #[cfg(test)]
@@ -184,6 +101,9 @@ mod tests {
     #[test]
     fn test_solve() {
         let result = solve(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, (Some(7), None));
+        assert_eq!(
+            result,
+            (Some(String::from("7")), Some(String::from("co,de,ka,ta")))
+        );
     }
 }
