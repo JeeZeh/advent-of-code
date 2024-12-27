890be1e34b602dd144a64a0e882fcf1958cdbaef
Jesse Ashmore <jesse.ashmore97@gmail.com>
2024-12-27 23:40:01 +0100

    wip(rs:2024-04): part 2

--- 2024-rs/src/bin/04.rs
+++ 2024-rs/src/bin/04.rs
@@ -1,6 +1,8 @@
-use std::collections::VecDeque;
+use std::{collections::VecDeque, fmt::Display};
 
-use advent_of_code::{lines_no_empty, Direction, DirectionAll, Grid};
+use advent_of_code::{
+    lines_no_empty, template::runner::Solution, Direction, DirectionAll, Grid, Pos2D,
+};
 use itertools::Itertools;
 
 advent_of_code::solution!(4);
@@ -16,7 +18,7 @@ fn find_xmas(search: &impl Grid<char>, from: (usize, usize)) -> usize {
     DirectionAll::iterator().for_each(|d| queue.push_front(('X', from, d)));
 
     while let Some((need, at, dir)) = queue.pop_back() {
-        if search.getxy_pos(from) == Some(&need) {
+        if search.getxy_pos(at) == Some(&need) {
             if need == 'S' {
                 count += 1;
                 continue;
@@ -36,13 +38,60 @@ fn find_xmas(search: &impl Grid<char>, from: (usize, usize)) -> usize {
     count
 }
 
-pub fn solve(input: &str) -> (Option<u64>, Option<u64>) {
+fn convolve(grid: &impl Grid<char>, kernel: &impl Grid<Option<char>>) -> i32 {
+    let mut matches = 0;
+    kernel.show_debug();
+    for y in 0..grid.height() - (kernel.height() - 1) {
+        for x in 0..grid.width() - (kernel.width() - 1) {
+            if match_kernel_at(grid, kernel, x, y) {
+                matches += 1;
+            }
+        }
+    }
+
+    matches
+}
+
+fn match_kernel_at(
+    grid: &impl Grid<char>,
+    kernel: &impl Grid<Option<char>>,
+    x: usize,
+    y: usize,
+) -> bool {
+    for (pos, e) in kernel.scan() {
+        let check = pos.add(&(x, y));
+        println!("{check:?} {:?} {:?}", grid.getxy_pos(check), e);
+        if e.is_some() && grid.getxy_pos(check) != e.as_ref() {
+            return false;
+        }
+    }
+
+    true
+}
+
+pub fn solve(input: &str) -> Solution<u64, usize> {
     let search = lines_no_empty(input)
         .map(|l| l.chars().collect_vec())
         .collect_vec();
 
-    let xmas_search_out = search.scan().map(|(at, _)| find_xmas(&search, at)).sum::<usize>();
-    (Some(xmas_search_out as u64), None)
+    let xmas_search_out = search
+        .scan()
+        .map(|(at, _)| find_xmas(&search, at))
+        .sum::<usize>();
+
+    let mut kernel: Vec<Vec<Option<char>>> = vec![
+        vec![Some('M'), None, Some('S')],
+        vec![None, Some('A'), None],
+        vec![Some('M'), None, Some('S')],
+    ];
+    let mut total = 0;
+    for _ in 0..3 {
+        total += convolve(&search, &kernel);
+        kernel = kernel.rot90();
+    }
+    total += convolve(&search, &kernel);
+
+    (Some(xmas_search_out as u64), Some(total as usize))
 }
 
 #[cfg(test)]
@@ -52,6 +101,6 @@ mod tests {
     #[test]
     fn test_solve() {
         let result = solve(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, (None, None));
+        assert_eq!(result, (Some(18), Some(9)));
     }
 }
