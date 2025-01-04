bc09fb892a87af4a53300816f6e0d2f7f9a4ab90
Jesse Ashmore <jesse.ashmore97@gmail.com>
2025-01-04 18:03:44 +0000

    optimize(rs:2024-05): remove Vec<T>

--- 2024-rs/src/bin/05.rs
+++ 2024-rs/src/bin/05.rs
@@ -1,3 +1,4 @@
+#![feature(iter_collect_into)]
 use std::{cmp::Ordering, collections::HashMap};
 
 use advent_of_code::lines_no_empty;
@@ -12,29 +13,52 @@ pub fn solve(input: &str) -> (Option<u64>, Option<u64>) {
         .map(|l| l.split_once("|").unwrap())
         .for_each(|(l, r)| rules[l.parse::<usize>().unwrap()] += 1 << r.parse::<u8>().unwrap());
 
-    let mut valid_mids = Vec::new();
-    let mut invalid_mids = Vec::new();
-    lines_no_empty(bottom)
-        .map(|l| l.split(",").map(|l| l.parse().unwrap()).collect_vec())
-        .map(|update| {
-            let sorted = update
-                .iter()
-                .copied()
-                .sorted_by(|&a, &b| sorted_rule(&rules, a, b))
-                // TODO(optimization): Remove collect().
-                .collect_vec();
-
-            match sorted == update {
-                true => valid_mids.push(update[update.len() / 2] as u64),
-                false => invalid_mids.push(sorted[sorted.len() / 2] as u64),
+    let mut valid_mids: u64 = 0;
+    let mut invalid_mids: u64 = 0;
+    for line in lines_no_empty(bottom) {
+        // Manually fill an array instead of allocating a Vec<T> every time
+        let mut stable_vec: [u8; 100] = [0; 100];
+        let mut ptr = 0;
+        let mut iter = line.split(",").map(|c| c.parse::<u8>());
+        while ptr < 100 {
+            if let Some(Ok(next)) = iter.next() {
+                stable_vec[ptr] = next;
+                ptr += 1;
+            } else {
+                break;
             }
-        })
-        .collect_vec();
+        }
 
-    (
-        Some(valid_mids.iter().sum()),
-        Some(invalid_mids.iter().sum()),
-    )
+        let mid = ptr / 2;
+        let sorted = stable_vec[..ptr]
+            .iter()
+            .sorted_by(|&&a, &&b| sorted_rule(&rules, a, b));
+        let mut is_sorted = true;
+        let mut mid_val = 0;
+        for (i, (&orig, &sort)) in stable_vec[..ptr].iter().zip_eq(sorted).enumerate() {
+            if orig != sort {
+                is_sorted = false;
+                // If we discover this needs to be fixed after passing the midpoint, we already know its value.
+                if i > mid {
+                    break;
+                }
+            }
+            if i == mid {
+                mid_val = sort;
+                // If we already know this needs to be fixed, we can immediately return the mid-value.
+                if !is_sorted {
+                    break;
+                }
+            }
+        }
+        if is_sorted {
+            valid_mids += mid_val as u64;
+        } else {
+            invalid_mids += mid_val as u64;
+        }
+    }
+
+    (Some(valid_mids), Some(invalid_mids))
 }
 
 fn sorted_rule(rules: &[u128], a: u8, b: u8) -> Ordering {
